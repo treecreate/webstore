f8a0fe2171e020743a1527744c0d0c7ec51f377d
Kwandes <bz.jasiek@gmail.com>
2021-03-02 20:41:51 +0100

    feat: 🎸 Add ResourceNotFoundException, Add dependencies

---
+++ apps/api/src/main/java/dk/treecreate/api/exceptionhandling/ResourceNotFoundException.java
@@ -0,0 +1,18 @@
+package dk.treecreate.api.exceptionhandling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public final class ResourceNotFoundException extends RuntimeException
+{
+    public ResourceNotFoundException()
+    {
+        super();
+    }
+
+    public ResourceNotFoundException(final String message)
+    {
+        super(message);
+    }
+}
