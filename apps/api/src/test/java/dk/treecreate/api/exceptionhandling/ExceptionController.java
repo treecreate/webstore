48aa865e4ad7a67ebe95a633d8c6fa71d9028628
Kwandes <bz.jasiek@gmail.com>
2021-03-04 23:20:34 +0100

    test: 💍 Add tests for API exceptions

---
+++ apps/api/src/test/java/dk/treecreate/api/exceptionhandling/ExceptionController.java
@@ -0,0 +1,35 @@
+/*
+    A helper class used by the exception handler testing for the purposes of mocking
+    Throws various exceptions based of the input
+ */
+
+package dk.treecreate.api.exceptionhandling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class ExceptionController
+{
+    @PostMapping("/exception")
+    public void getSpecificException(@RequestParam(name = "exceptionName") String exceptionName,
+                                     @RequestParam(required = false, name = "customMessage") String customMessage)
+    {
+        switch (exceptionName)
+        {
+            case "ResponseStatusException":
+              if (customMessage != null)
+                throw new ResponseStatusException(HttpStatus.I_AM_A_TEAPOT,customMessage);
+              else
+                throw new ResponseStatusException(HttpStatus.I_AM_A_TEAPOT);
+            case "ResourceNotFoundException":
+                if (customMessage != null)
+                    throw new ResourceNotFoundException(customMessage);
+                else
+                    throw new ResourceNotFoundException();
+        }
+    }
+}
