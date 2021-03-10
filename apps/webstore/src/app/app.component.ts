22ce8a7add07817cf8459d3c532e1e8da2431e87
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-03-10 15:14:46 +0100

    feat: 🎸 add google analytics with Router event tracking
    google analytics has been added correctly. Since Angular applications
    are single page applications, it is not yet tracking user interaction or
    flow. Therefore, i added Router checking, so when the app.component.ts
    detects router change, it report it to the google analytics. Later I
    will add a google analytics service file that does 'event' checking for
    more detailed user info.
    
    BREAKING CHANGE: 🧨 none

--- apps/webstore/src/app/app.component.ts
+++ apps/webstore/src/app/app.component.ts
@@ -1,10 +1,31 @@
+import { getHtmlTagDefinition } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router'; 
+
+//Google analytics 
+declare let gtag: Function;
 
 @Component({
   selector: 'webstore-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
+
+  constructor(public router: Router){
+    
+    //Google analytics
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd){
+        gtag('config', 'G-4VY53TX2KS', 
+          {
+            'page_path': event.urlAfterRedirects
+          }
+        );
+      }
+    })
+  }
+  
   title = 'webstore';
 }
