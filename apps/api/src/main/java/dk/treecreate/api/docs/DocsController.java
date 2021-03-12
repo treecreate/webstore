1d8b9598b99f999e3e51694eb44ee26816c3555e
Kwandes <bz.jasiek@gmail.com>
2021-03-12 22:10:21 +0100

    feat: 🎸 Add a redirect for docs url

---
+++ apps/api/src/main/java/dk/treecreate/api/docs/DocsController.java
@@ -0,0 +1,23 @@
+/*
+  Controller methods related to the Swagger UI documentation
+ */
+package dk.treecreate.api.docs;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
+
+@Controller
+@ApiIgnore
+@RequestMapping("/docs")
+public class DocsController
+{
+
+  // a simple redirect to the docs page. A simple quality of life improvement
+  @GetMapping("")
+  public String swaggerDocs()
+  {
+    return "redirect:/swagger-ui/";
+  }
+}
