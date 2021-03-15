2d2e635e3ac17988cd5ba8dc8360fddbc518cfc1
Kwades <bz.jasiek@gmail.com>
2021-03-15 20:02:01 +0100

    feat: 🎸 Add apiUrl and gtag to environments, implement env.js

--- apps/webstore/src/environments/environment.prod.ts
+++ apps/webstore/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
-export const environment = {
+import { IEnvironment } from './ienvironment';
+
+// production-specific environment
+export const environment: IEnvironment = {
   production: true,
+  envName: 'production',
+
+  apiUrl: window['env'] ? window['env']['apiUrl'] : 'http://localhost:5000',
+  gtag: window['env'] ? window['env']['gtag'] : 'gtag-for-prod',
 };
