73d3896136412b641d1a9b19f5d15090898fca60
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-03-16 16:57:27 +0100

    feat: 🎸 Added footer
    Fully styled footer. Links to email, facebook and instagram. non
    functional sign up form for the newsletter. button stylings in
    app/styles.css for use through the whole webstore.

--- apps/webstore/src/app/app.module.ts
+++ apps/webstore/src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { GoogleAnalyticsService } from './google-analytics/google-analytics.service';
+import { GoogleAnalyticsService } from './services/google-analytics/google-analytics.service';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FooterComponent],
   imports: [
     BrowserModule,
+    MDBBootstrapModule.forRoot(),
     RouterModule.forRoot([], {
       initialNavigation: 'enabled',
     }),
   ],
   providers: [GoogleAnalyticsService],
   bootstrap: [AppComponent],
-  exports: [],
+  exports: [FooterComponent],
 })
 export class AppModule {}
---
+++ apps/webstore/src/app/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'webstore-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+})
+export class FooterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
