6b17f912cc8dea355aac8c2a58a467100114b3e2
Kwandes <bz.jasiek@gmail.com>
2021-03-16 13:26:05 +0100

    fix: 🐛 Add a missing env.template.js file

---
+++ apps/webstore/src/assets/env.template.js
@@ -0,0 +1,9 @@
+// This file is used by the dockerfile and replaces the normal env.js with values obtained from the environment
+(function (window) {
+    window['env'] = window['env'] || {};
+  
+    // Environment variables
+    window['env']['apiUrl'] = '${API_URL}';
+    window['env']['gtag'] = '${GTAG}';
+  })(this);
+  
