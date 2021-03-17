d10be7aed40e1e3903a68255b8e299c71e9aa2cd
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-03-17 18:15:09 +0100

    feat: 🎸 Added navbar and minor footer styling
    Minor styling changes to the footer. Navbar implemented with styling.
    Works for mobile and desktop. NO ROUTING. I will possibly make a new
    navbar for the mobile version that contains icons, next time.

--- apps/webstore/src/app/app.module.ts
+++ apps/webstore/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { GoogleAnalyticsService } from './services/google-analytics/google-analytics.service';
 import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent],
+  declarations: [AppComponent, FooterComponent, NavbarComponent],
   imports: [
     BrowserModule,
     MDBBootstrapModule.forRoot(),
@@ -18,6 +19,6 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   providers: [GoogleAnalyticsService],
   bootstrap: [AppComponent],
-  exports: [FooterComponent],
+  exports: [FooterComponent, NavbarComponent],
 })
 export class AppModule {}
---
+++ apps/webstore/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'webstore-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
