db322658d67f68128a6c2ae2fd8253b8a4e43304
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-03-17 22:23:28 +0100

    feat: 🎸 Routing with page components
    Created home, basket, product, pageNotFound. Added routing in the
    navbar.

--- apps/webstore/src/app/app.module.ts
+++ apps/webstore/src/app/app.module.ts
@@ -2,23 +2,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
+// COMPONENTS
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { GoogleAnalyticsService } from './services/google-analytics/google-analytics.service';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
+// PAGES
+import { HomeComponent } from './public/home/home.component';
+import { ProductComponent } from './public/product/product.component';
+import { BasketComponent } from './public/basket/basket.component';
+import { PageNotFoundComponent } from './public/page-not-found/page-not-found.component';
+
 @NgModule({
-  declarations: [AppComponent, FooterComponent, NavbarComponent],
+  declarations: [
+    AppComponent, 
+    FooterComponent, 
+    NavbarComponent, 
+    HomeComponent, 
+    ProductComponent, 
+    BasketComponent, 
+    PageNotFoundComponent],
   imports: [
     BrowserModule,
     MDBBootstrapModule.forRoot(),
-    RouterModule.forRoot([], {
+    RouterModule.forRoot([
+      { path:'home', component:HomeComponent},
+      { path:'product', component:ProductComponent },
+      { path:'basket', component:BasketComponent },
+      { path:'', pathMatch:'full', redirectTo:'home' }, // Redirect to home page 
+      { path:'**', component:PageNotFoundComponent }    // PageNotFound for all other page requests
+    ], {
       initialNavigation: 'enabled',
     }),
   ],
   providers: [GoogleAnalyticsService],
   bootstrap: [AppComponent],
-  exports: [FooterComponent, NavbarComponent],
+  exports: [
+    FooterComponent, 
+    NavbarComponent, 
+    HomeComponent, 
+    ProductComponent, 
+    BasketComponent, 
+    PageNotFoundComponent],
 })
 export class AppModule {}
