767d7eee0de9aef7f6edc5b87ef19a482fa3d056
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-03-17 22:09:54 +0100

    feat: 🎸 Changin styling and testing navbar

--- apps/webstore/src/app/components/navbar/navbar.component.ts
+++ apps/webstore/src/app/components/navbar/navbar.component.ts
@@ -10,6 +10,15 @@ export class NavbarComponent implements OnInit {
   constructor() {
   }
 
+  backetItemOptions(amount:Number) : string {
+    if(amount===0){
+      return "Basket empty";
+    } else {
+      return '(' + amount + ') products ';
+    }
+  };
+  
+
   ngOnInit(): void {
   }
 
