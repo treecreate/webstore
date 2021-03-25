06e97e7e147a8c222eb678ef36fbb21f12a4ac51
Kwades <bz.jasiek@gmail.com>
2021-03-25 19:15:54 +0100

    refactor: 💡 Revert all changes done on the branch
    Done with `git revert --no-commit HEAD~15..`. Teo decided that it would
    be easier to just start from scratch

--- apps/webstore/src/app/app.component.spec.ts
+++ apps/webstore/src/app/app.component.spec.ts
@@ -1,51 +1,12 @@
-// main app import
-import { AppComponent } from './app.component';
-
-// imported libraries
 import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
-// components
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-
-// pages
-import { AboutUsComponent } from './public/about-us/about-us.component';
-import { BasketComponent } from './public/basket/basket.component';
-import { ErrorComponent } from './public/issue-pages/error/error.component';
-import { HomeComponent } from './public/home/home.component';
-import { PageNotFoundComponent } from './public/issue-pages/page-not-found/page-not-found.component';
-import { PrivacyNoticeComponent } from './public/terms-and-conditions/privacy-notice/privacy-notice.component';
-import { ProductComponent } from './public/product/product.component';
-import { ProfileComponent } from './public/profile/profile.component';
-import { TermsOfPaymentComponent } from './public/terms-and-conditions/terms-of-payment/terms-of-payment.component';
-import { ResetPasswordComponent } from './public/issue-pages/reset-password/reset-password.component';
-import { IntroductionComponent } from './public/product/introduction/introduction.component';
-
-// service import
-import { GoogleAnalyticsService } from './services/google-analytics/google-analytics.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, MDBBootstrapModule],
-      declarations: [
-        AppComponent,
-        FooterComponent,
-        NavbarComponent,
-        HomeComponent,
-        ProductComponent,
-        BasketComponent,
-        PageNotFoundComponent,
-        ProfileComponent,
-        AboutUsComponent,
-        PrivacyNoticeComponent,
-        TermsOfPaymentComponent,
-        ErrorComponent,
-        ResetPasswordComponent,
-        IntroductionComponent,
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -59,5 +20,14 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('webstore');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Welcome to webstore!'
+    );
   });
 });
--- apps/webstore/src/app/app.module.ts
+++ apps/webstore/src/app/app.module.ts
@@ -1,47 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
-// COMPONENTS
 import { AppComponent } from './app.component';
-import { GoogleAnalyticsService } from './services/google-analytics/google-analytics.service';
-import { FooterComponent } from './components/footer/footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-
-// PAGES
-import { HomeComponent } from './public/home/home.component';
-import { ProductComponent } from './public/product/product.component';
-import { BasketComponent } from './public/basket/basket.component';
-import { PageNotFoundComponent } from './public/issue-pages/page-not-found/page-not-found.component';
-import { ProfileComponent } from './public/profile/profile.component';
-import { AboutUsComponent } from './public/about-us/about-us.component';
-import { PrivacyNoticeComponent } from './public/terms-and-conditions/privacy-notice/privacy-notice.component';
-import { TermsOfPaymentComponent } from './public/terms-and-conditions/terms-of-payment/terms-of-payment.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ErrorComponent } from './public/issue-pages/error/error.component';
-import { ResetPasswordComponent } from './public/issue-pages/reset-password/reset-password.component';
-import { IntroductionComponent } from './public/product/introduction/introduction.component';
+import { RouterModule } from '@angular/router';
+import { GoogleAnalyticsService } from './google-analytics/google-analytics.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
-    NavbarComponent,
-    HomeComponent,
-    ProductComponent,
-    BasketComponent,
-    PageNotFoundComponent,
-    ProfileComponent,
-    AboutUsComponent,
-    PrivacyNoticeComponent,
-    TermsOfPaymentComponent,
-    ErrorComponent,
-    ResetPasswordComponent,
-    IntroductionComponent,
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], {
+      initialNavigation: 'enabled',
+    }),
   ],
-  imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
   providers: [GoogleAnalyticsService],
   bootstrap: [AppComponent],
-  exports: [IntroductionComponent],
+  exports: [],
 })
 export class AppModule {}
