d4f760e537765190bc70d2814086865998dc649e
Cristian <cristi.valentin29@gmail.com>
2021-03-25 17:26:49 +0100

    feat: 🎸 Combine localization with local storage

--- apps/webstore/src/app/app.component.ts
+++ apps/webstore/src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Router, NavigationEnd } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import { LocaleType } from './i18n';
+import { LocalStorageService, LocalStorageVars } from './services/local-storage';
 
 //Google analytics
 declare let gtag: Function;
@@ -9,14 +13,34 @@ declare let gtag: Function;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
+  currentLocale$: BehaviorSubject<LocaleType>; 
+  localeCode;
+
   localesList = [
     { code: 'en-US', label: 'English' },
     { code: 'dk', label: 'Danish' }
-  ]
-
+  ];
   title = $localize`webstore`;
-  constructor(public router: Router) {
+
+  constructor(public router: Router, private localStorageService: LocalStorageService,
+     @Inject(DOCUMENT) private document: Document) {
+
+    // get the default/pre-existing locale from the localStorage
+    this.currentLocale$ = this.localStorageService.getItem<LocaleType>(LocalStorageVars.locale);
+
+    this.localeCode = this.document.location.href.search( "/dk" ) ? LocaleType.en : LocaleType.dk;
+    console.log( this.localeCode );
+
+    this.currentLocale$ = this.localStorageService
+      .setItem<LocaleType>( LocalStorageVars.locale, this.localeCode );
+
+    // set the class variable to automatically update when the locale changes
+    this.currentLocale$.subscribe(() => {
+      console.log('Locale got changed: ' + this.currentLocale$.getValue());
+    });
+
     //Google analytics
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
@@ -25,6 +49,6 @@ export class AppComponent {
         });
       }
     });
-  }
 
+  }
 }
