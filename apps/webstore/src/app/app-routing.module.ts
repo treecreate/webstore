2382a299181524928e52f4f48debbddf8555ed22
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-03-31 20:31:44 +0200

    feat: 🎸 Added page not found. Missin mobile styling

--- apps/webstore/src/app/app-routing.module.ts
+++ apps/webstore/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/issues/page-not-found/page-not-found.component';
 
-const routes: Routes = [{ path: 'home', component: HomeComponent }];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' }, // Redirect to home page
+  { path: '**', component: PageNotFoundComponent }, // PageNotFound for all other page requests
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- apps/webstore/src/app/app.module.ts
+++ apps/webstore/src/app/app.module.ts
@@ -11,12 +11,19 @@ import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PageNotFoundComponent } from './pages/issues/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, FooterComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [GoogleAnalyticsService],
   bootstrap: [AppComponent],
-  exports: [HomeComponent],
+  exports: [HomeComponent, PageNotFoundComponent],
 })
 export class AppModule {}
