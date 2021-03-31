c0a808167a24ebca7a46f30112b85890a273d280
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-03-31 14:01:22 +0200

    feat: 🎸 Created Footer without MDB
    Added the button styling win index.html so it is global in our project.
    Theres a description on how to use it in docs. The footer links are not
    prepared yet.

--- apps/webstore/src/app/shared/components/footer/footer.component.ts
+++ apps/webstore/src/app/shared/components/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'webstore-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css'],
+  styleUrls: ['./footer.component.css', ], //'../../../../assets/styles/TCButtons.css'
 })
 export class FooterComponent implements OnInit {
   constructor() {}
