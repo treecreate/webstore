5d02b45135d68fbdb51d6bd699126124f351a375
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-04-01 11:36:30 +0200

    fix: 🐛 Fixed the test that where failing

--- apps/webstore/src/app/app.component.spec.ts
+++ apps/webstore/src/app/app.component.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { PageNotFoundComponent } from './pages/issues/page-not-found/page-not-found.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [RouterTestingModule, NgbModule],
+      declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        FooterComponent,
+        PageNotFoundComponent,
+      ],
     }).compileComponents();
   });
 
--- apps/webstore/src/app/shared/components/navbar/navbar.component.spec.ts
+++ apps/webstore/src/app/shared/components/navbar/navbar.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -9,6 +11,7 @@ describe('NavbarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [NavbarComponent],
+      imports: [NgbModule, RouterTestingModule],
     }).compileComponents();
   });
 
