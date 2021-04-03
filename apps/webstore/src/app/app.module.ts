e7d91ab6edb8f8413ddfb7a6a8ad24e39124e0b6
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-04-03 22:02:22 +0200

    fix: 🐛 Fix app.module import issue

--- apps/webstore/src/app/app.module.ts
+++ apps/webstore/src/app/app.module.ts
@@ -31,5 +31,6 @@ import { HomeComponent } from './pages/home/home.component';
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [GoogleAnalyticsService, NgbActiveModal],
   bootstrap: [AppComponent],
+  exports: [ CookiePromptModalComponent ]
 })
 export class AppModule {}
