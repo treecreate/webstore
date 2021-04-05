60c87c442f5344fa4a3f9d210c93aa5f480ca4c8
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-04-05 11:04:22 +0200

    fix: 🐛 KYS

--- apps/webstore/src/app/shared/components/navbar/navbar.component.spec.ts
+++ apps/webstore/src/app/shared/components/navbar/navbar.component.spec.ts
@@ -1,7 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbActiveModal,
+  NgbModal,
+  NgbModule,
+} from '@ng-bootstrap/ng-bootstrap';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('NavbarComponent', () => {
@@ -11,6 +15,7 @@ describe('NavbarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [NavbarComponent],
+      providers: [NgbModal, NgbActiveModal],
       imports: [NgbModule, RouterTestingModule],
     }).compileComponents();
   });
