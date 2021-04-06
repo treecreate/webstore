5ae7eb6c3972ceaf9e09df3727004dc53a4f23c3
alex <57399961+IroniX2@users.noreply.github.com>
2021-04-06 02:47:59 +0200

    feat: 🎸 Add healthcheck endpoint and its associated mock test
    ✅ Closes: #18

---
+++ apps/api/src/main/java/dk/treecreate/api/healthcheck/HealthcheckController.java
@@ -0,0 +1,25 @@
+package dk.treecreate.api.healthcheck;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController()
+@RequestMapping("/healthcheck")
+public class HealthcheckController
+{
+  @GetMapping("")
+  @ResponseStatus(HttpStatus.OK)
+  public Map<String, String> healthCheck()
+  {
+    // HashMap can be changed to Json Object if needed.
+    Map<String, String> map = new HashMap<>();
+    map.put("status", "200");
+    map.put("message", "Server is live");
+    return map;
+  }
+}
---
+++ apps/api/src/test/java/dk/treecreate/api/healthcheck/HealthcheckTests.java
@@ -0,0 +1,41 @@
+package dk.treecreate.api.healthcheck;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(value = HealthcheckController.class)
+class HealthcheckTests {
+
+  @Autowired
+  private MockMvc mvc;
+
+  @Test
+    // MockMvc throws Exception, so i must catch it
+  void healthcheckReturnedStatusTest() throws Exception {
+    mvc.perform(
+      get("/healthcheck"))
+      .andExpect(status().isOk());
+  }
+
+  @Test
+    // MockMvc throws Exception, so i must catch it
+  void healthcheckBodyTest() throws Exception {
+    mvc.perform(get("/healthcheck")
+      // Ensure it is a Json
+      .contentType(MediaType.APPLICATION_JSON))
+      // Check the contents of the body.
+      .andExpect(jsonPath("status", is("200")))
+      .andExpect(jsonPath("message", is("Server is live")));
+  }
+}
