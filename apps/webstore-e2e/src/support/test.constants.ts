2201dfb6e0cb0e438f40ec9c3d958b327def0a27
Kwades <bz.jasiek@gmail.com>
2021-04-07 21:41:17 +0200

    test: 💍 Add e2e tests for the cookie prompt

---
+++ apps/webstore-e2e/src/support/test.constants.ts
@@ -0,0 +1,17 @@
+// Various enums from the webstore app
+// ToDo - extract them into a library that all projects can use
+export enum LocalStorageVars {
+  locale = 'locale',
+  cookiesAccepted = 'cookiesAccepted',
+}
+
+export enum CookieStatus {
+  accepted = 'accepted',
+  rejected = 'rejected',
+  undefined = 'undefined',
+}
+
+export enum LocaleType {
+  dk = 'dk',
+  en = 'en-US',
+}
