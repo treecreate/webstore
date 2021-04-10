83522a6b448db1dda55cf358ecc0c294a0b35961
Cristian <cristi.valentin29@gmail.com>
2021-04-10 19:34:11 +0200

    feat: 🎸 Add button to change locale in NavBar
    Add a button in the NavBar that changes the locale and the localization
    environment variable

--- apps/webstore/src/app/shared/components/navbar/navbar.component.ts
+++ apps/webstore/src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
+import { LocaleType } from '../../../i18n';
+import { LocalStorageService, LocalStorageVars } from '../../services/local-storage';
 
 @Component({
   selector: 'webstore-navbar',
@@ -7,6 +11,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   public isMenuCollapsed = true;
+  locale$: BehaviorSubject<LocaleType>;
+  localeCode: LocaleType;
 
   basketItemOptions(amount: Number): string {
     if (amount === 0) {
@@ -15,7 +21,26 @@ export class NavbarComponent implements OnInit {
     return `(${amount}) products `;
   }
 
-  constructor() {}
+  constructor(private localStorageService: LocalStorageService, 
+    @Inject(DOCUMENT) private document: Document) {
+    this.locale$ = this.localStorageService.getItem<LocaleType>(LocalStorageVars.locale);
+    this.localeCode = this.locale$.getValue();
+  
+    this.locale$.subscribe(() => {
+      console.log('Locale changed to: ' + this.locale$.getValue());
+    });
+  }
+
+  changeLocale() {
+    if ( this.localeCode === LocaleType.en ){
+      this.localeCode = LocaleType.dk;
+    }
+    else {
+      this.localeCode = LocaleType.en;
+    }
+    
+    this.locale$ = this.localStorageService.setItem<LocaleType>( LocalStorageVars.locale, this.localeCode );
+  }
 
   ngOnInit(): void {}
 }
