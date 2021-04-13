829182098c1194274f04bfd9ad6d22b363ee54e9
Alex M <57399961+IroniX2@users.noreply.github.com>
2021-04-13 16:13:06 +0200

    Add a suppress warning for the missing UUID long of custom exception

--- apps/api/src/main/java/dk/treecreate/api/exceptionhandling/ResourceNotFoundException.java
+++ apps/api/src/main/java/dk/treecreate/api/exceptionhandling/ResourceNotFoundException.java
@@ -3,6 +3,7 @@ package dk.treecreate.api.exceptionhandling;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@SuppressWarnings("serial")
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public final class ResourceNotFoundException extends RuntimeException
 {
