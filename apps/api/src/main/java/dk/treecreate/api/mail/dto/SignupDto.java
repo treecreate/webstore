07ed39129fa5729c281a08e6b33cc0a4d1bbb289
Kwandes <bz.jasiek@gmail.com>
2021-04-15 17:37:21 +0200

    refactor: 💡 Remove lombok and add getters and setters to DTOs

--- apps/api/src/main/java/dk/treecreate/api/mail/dto/SignupDto.java
+++ apps/api/src/main/java/dk/treecreate/api/mail/dto/SignupDto.java
@@ -2,17 +2,23 @@ package dk.treecreate.api.mail.dto;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.validation.constraints.Size;
 
-@Getter
-@Setter
 @ApiModel(description = "Information needed to send out a signup email")
 public class SignupDto
 {
   @Size(min = 5, max = 255)
   @ApiModelProperty(notes = "Email address to send a message to", example = "user@yahoo.mail")
   String email;
+
+  public String getEmail()
+  {
+    return email;
+  }
+
+  public void setEmail(String email)
+  {
+    this.email = email;
+  }
 }
