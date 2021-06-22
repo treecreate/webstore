1ac4e60f8ee3e282e0860bbbb72162898be8e696
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-06-22 13:34:08 +0200

    style: 💄 formatting

--- apps/webstore/src/app/pages/product/product.component.ts
+++ apps/webstore/src/app/pages/product/product.component.ts
@@ -3,52 +3,52 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'webstore-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss', '../../../assets/styles/tc-input-field.scss']
+  styleUrls: [
+    './product.component.scss',
+    '../../../assets/styles/tc-input-field.scss',
+  ],
 })
 export class ProductComponent implements OnInit {
+  fontCollection = ['times new roman', 'roboto', 'Helvetica'];
 
-  fontCollection = ["times new roman", "roboto", "Helvetica"]
+  designTitle: string = 'Untitled-1';
+  font: string = 'times new roman';
+  design: string = 'first';
+  boxSize: number = 10;
+  banner: boolean = false;
+  bigFont: boolean = false;
 
-  designTitle: string = "Untitled-1"
-  font: string = "times new roman" 
-  design: string = "first"
-  boxSize: number = 10; 
-  banner: boolean = false; 
-  bigFont: boolean = false; 
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   nextDesign() {
-    if (this.design === "first") {
-      this.design = "second"
+    if (this.design === 'first') {
+      this.design = 'second';
     } else {
-      this.design = "first"
+      this.design = 'first';
     }
   }
 
   nextFont() {
-    let selectedFont = this.fontCollection.indexOf(this.font)
+    let selectedFont = this.fontCollection.indexOf(this.font);
     if (selectedFont > 1) {
-      this.font = this.fontCollection[0]
+      this.font = this.fontCollection[0];
     } else {
-      this.font = this.fontCollection[selectedFont+1]
+      this.font = this.fontCollection[selectedFont + 1];
     }
   }
 
   prevFont() {
-    let selectedFont = this.fontCollection.indexOf(this.font)
+    let selectedFont = this.fontCollection.indexOf(this.font);
     if (selectedFont < 1) {
-      this.font = this.fontCollection[2]
+      this.font = this.fontCollection[2];
     } else {
-      this.font = this.fontCollection[selectedFont-1]
+      this.font = this.fontCollection[selectedFont - 1];
     }
   }
 
   onKey(event: any) {
-    this.designTitle = event.target.value; 
+    this.designTitle = event.target.value;
   }
-
 }
