f2647669d6f98ac0aa5f758547cf9c970d1cd294
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-06-22 13:15:51 +0200

    feat: 🎸 add the last options to the desktop menu
    Finished the options menu for desktop. still missing the mobile version.

--- apps/webstore/src/app/pages/product/product.component.ts
+++ apps/webstore/src/app/pages/product/product.component.ts
@@ -10,7 +10,7 @@ export class ProductComponent implements OnInit {
   fontCollection = ["times new roman", "roboto", "Helvetica"]
 
   designTitle: string = "Untitled-1"
-  font: string = "times new roman"
+  font: string = "times new roman" 
   design: string = "first"
   boxSize: number = 10; 
   banner: boolean = false; 
@@ -27,6 +27,28 @@ export class ProductComponent implements OnInit {
     } else {
       this.design = "first"
     }
+  }
+
+  nextFont() {
+    let selectedFont = this.fontCollection.indexOf(this.font)
+    if (selectedFont > 1) {
+      this.font = this.fontCollection[0]
+    } else {
+      this.font = this.fontCollection[selectedFont+1]
+    }
+  }
+
+  prevFont() {
+    let selectedFont = this.fontCollection.indexOf(this.font)
+    if (selectedFont < 1) {
+      this.font = this.fontCollection[2]
+    } else {
+      this.font = this.fontCollection[selectedFont-1]
+    }
+  }
+
+  onKey(event: any) {
+    this.designTitle = event.target.value; 
   }
 
 }
