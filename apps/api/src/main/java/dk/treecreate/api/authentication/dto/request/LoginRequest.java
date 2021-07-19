aa5d62be2bc8b88483f28160eab39a80e5fc6c35
Kwandes <bz.jasiek@gmail.com>
2021-07-19 22:11:37 +0200

    test: 💍 Add tests for `/auth/signup` endpoint

--- apps/api/src/main/java/dk/treecreate/api/authentication/dto/request/LoginRequest.java
+++ apps/api/src/main/java/dk/treecreate/api/authentication/dto/request/LoginRequest.java
@@ -17,7 +17,7 @@ public class LoginRequest
         return email;
     }
 
-    public void setUsername(String email)
+    public void setEmail(String email)
     {
         this.email = email;
     }
--- apps/api/src/test/java/dk/treecreate/api/authentication/AuthControllerTests.java
+++ apps/api/src/test/java/dk/treecreate/api/authentication/AuthControllerTests.java
@@ -2,9 +2,11 @@ package dk.treecreate.api.authentication;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dk.treecreate.api.authentication.dto.request.LoginRequest;
+import dk.treecreate.api.authentication.dto.request.SignupRequest;
 import dk.treecreate.api.authentication.models.ERole;
 import dk.treecreate.api.authentication.models.Role;
 import dk.treecreate.api.authentication.models.User;
+import dk.treecreate.api.authentication.repository.RoleRepository;
 import dk.treecreate.api.authentication.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +25,7 @@ import java.util.Set;
 import java.util.UUID;
 
 import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -38,6 +41,11 @@ class AuthControllerTests
     @MockBean
     private UserRepository userRepository;
 
+    @MockBean
+    private RoleRepository roleRepository;
+
+    // region Signin
+
     @Test
     @DisplayName("/auth/signin endpoint return 400 when the request body is incorrect")
     void signinReturnsBadRequestOnInvalidBody() throws Exception
@@ -52,7 +60,7 @@ class AuthControllerTests
     void signinReturnsUnauthorizedOnInvalidCredentials() throws Exception
     {
         LoginRequest loginRequest = new LoginRequest();
-        loginRequest.setUsername("test@treecreate.dk");
+        loginRequest.setEmail("test@treecreate.dk");
         loginRequest.setPassword("abcDEF1234");
 
         User user = new User();
@@ -80,7 +88,7 @@ class AuthControllerTests
     void signinCorrectlySignsInUser() throws Exception
     {
         LoginRequest loginRequest = new LoginRequest();
-        loginRequest.setUsername("test@treecreate.dk");
+        loginRequest.setEmail("test@treecreate.dk");
         loginRequest.setPassword("abcDEF123");
 
         User user = new User();
@@ -106,6 +114,116 @@ class AuthControllerTests
             .andExpect(jsonPath("tokenType", is("Bearer")))
             .andExpect(jsonPath("accessToken", is(notNullValue())));
     }
+
+    //endregion
+
+    //region Sign Up
+
+    @Test
+    @DisplayName("/auth/signup endpoint return 400 when the request body is incorrect")
+    void signupReturnsBadRequestOnInvalidBody() throws Exception
+    {
+        mvc.perform(post("/auth/signup")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("/auth/signup endpoint return 400 when user email ios already taken")
+    void signupReturnsBadRequestWhenUserEmailIsTaken() throws Exception
+    {
+        SignupRequest signupRequest = new SignupRequest();
+        signupRequest.setEmail("test@treecreate.dk");
+        signupRequest.setPassword("abcDEF123");
+
+        Mockito.when(userRepository.existsByEmail(signupRequest.getEmail())).thenReturn(true);
+
+        mvc.perform(post("/auth/signup")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(asJsonString(signupRequest)))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("/auth/signup endpoint correctly creates a new user")
+    void signupCorrectlyCreatesNewUser() throws Exception
+    {
+        Set<Role> roles = new HashSet<>();
+        roles.add(new Role(ERole.ROLE_USER));
+
+        SignupRequest signupRequest = new SignupRequest();
+        signupRequest.setEmail("test@treecreate.dk");
+        signupRequest.setPassword("abcDEF123");
+        User user = new User();
+        user.setUserId(UUID.fromString("c0a80121-7ab6-1787-817a-b69966240000"));
+        user.setEmail(signupRequest.getEmail());
+        user.setUsername(signupRequest.getEmail());
+        user.setPassword(
+            "$2a$10$ZPr0bH6kt2EnjkkRk1TEH.Mnyo/GRlfjBj/60gFuLI/BnauOx2p62"); // hashed version of "abcDEF123"
+        user.setRoles(roles);
+
+
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+        Mockito.when(roleRepository.findByName(ERole.ROLE_USER)).thenReturn(
+            java.util.Optional.of(new Role(ERole.ROLE_USER)));
+
+        mvc.perform(post("/auth/signup")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(asJsonString(signupRequest)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("userId",
+                is(nullValue()))) // normally a proper ID is returned but not when mocked like this
+            .andExpect(jsonPath("username", is(user.getEmail())))
+            .andExpect(jsonPath("email", is(user.getEmail())))
+            .andExpect(jsonPath("$.roles", hasSize(1)));
+    }
+
+    @Test
+    @DisplayName("/auth/signup endpoint correctly creates a new user with specified roles")
+    void signupCorrectlyCreatesNewUserWithSpecifiedRoles() throws Exception
+    {
+        Set<Role> roles = new HashSet<>();
+        roles.add(new Role(ERole.ROLE_USER));
+        roles.add(new Role(ERole.ROLE_DEVELOPER));
+        roles.add(new Role(ERole.ROLE_ADMIN));
+
+        Set<String> strRoles = new HashSet<>();
+        strRoles.add("user");
+        strRoles.add("developer");
+        strRoles.add("admin");
+
+        SignupRequest signupRequest = new SignupRequest();
+        signupRequest.setEmail("test@treecreate.dk");
+        signupRequest.setPassword("abcDEF123");
+        signupRequest.setRoles(strRoles);
+
+        User user = new User();
+        user.setUserId(UUID.fromString("c0a80121-7ab6-1787-817a-b69966240000"));
+        user.setEmail(signupRequest.getEmail());
+        user.setUsername(signupRequest.getEmail());
+        user.setPassword(
+            "$2a$10$ZPr0bH6kt2EnjkkRk1TEH.Mnyo/GRlfjBj/60gFuLI/BnauOx2p62"); // hashed version of "abcDEF123"
+        user.setRoles(roles);
+
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+        Mockito.when(roleRepository.findByName(ERole.ROLE_USER)).thenReturn(
+            java.util.Optional.of(new Role(ERole.ROLE_USER)));
+        Mockito.when(roleRepository.findByName(ERole.ROLE_DEVELOPER)).thenReturn(
+            java.util.Optional.of(new Role(ERole.ROLE_DEVELOPER)));
+        Mockito.when(roleRepository.findByName(ERole.ROLE_ADMIN)).thenReturn(
+            java.util.Optional.of(new Role(ERole.ROLE_ADMIN)));
+
+        mvc.perform(post("/auth/signup")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(asJsonString(signupRequest)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("userId",
+                is(nullValue()))) // normally a proper ID is returned but not when mocked like this
+            .andExpect(jsonPath("username", is(user.getEmail())))
+            .andExpect(jsonPath("email", is(user.getEmail())))
+            .andExpect(jsonPath("$.roles", hasSize(3)));
+    }
+    //endregion
 
     public static String asJsonString(final Object obj)
     {
