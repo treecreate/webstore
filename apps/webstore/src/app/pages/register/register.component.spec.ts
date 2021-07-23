1df6ecaabed7c8d1f54df9b972be9c3f439f87ab
Kwades <bz.jasiek@gmail.com>
2021-07-23 17:50:11 +0200

    test: 💍 Add missing test imports

--- apps/webstore/src/app/pages/register/register.component.spec.ts
+++ apps/webstore/src/app/pages/register/register.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { RegisterComponent } from './register.component';
 
@@ -11,7 +12,7 @@ describe('RegisterComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
-      imports: [HttpClientModule, FormsModule],
+      imports: [HttpClientModule, FormsModule, RouterTestingModule],
     }).compileComponents();
   });
 
