7b8904d868cbce679f119c8225244ad6c96593ef
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-07-30 00:18:42 +0200

    feat: 🎸 Creating form in account info page and adding styling

--- apps/webstore/src/app/pages/auth/profile/profile.component.ts
+++ apps/webstore/src/app/pages/auth/profile/profile.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { IUser } from '@interfaces';
+import { ToastService } from '../../../shared/components/toast/toast-service';
 import { UserService } from '../../../shared/services/user/user.service';
 
 @Component({
   selector: 'webstore-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css'],
+  styleUrls: [
+    './profile.component.css',
+    '../../../../assets/styles/tc-input-field.scss',
+  ],
 })
 export class ProfileComponent implements OnInit {
   currentUser: IUser;
+  accountInfoForm: FormGroup;
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private toastService: ToastService
+  ) {}
 
   // TODO: implement proper profile stuff
 
@@ -24,5 +33,15 @@ export class ProfileComponent implements OnInit {
       console.error(error);
       // TODO: handle failed fetching of the data
     }
+
+    this.accountInfoForm = new FormGroup({
+      name: new FormControl(''),
+      phone: new FormControl(''),
+      email: new FormControl(''),
+      streetOne: new FormControl(''),
+      streetTwo: new FormControl(''),
+      city: new FormControl(''),
+      postalCode: new FormControl(''),
+    });
   }
 }
