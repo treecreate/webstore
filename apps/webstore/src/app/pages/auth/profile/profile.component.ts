fb429b05e728399cd2e9b8fbe967e641c6c9ff2c
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-07-30 13:24:27 +0200

    feat: 🎸 Update form values with the currentUser

--- apps/webstore/src/app/pages/auth/profile/profile.component.ts
+++ apps/webstore/src/app/pages/auth/profile/profile.component.ts
@@ -15,6 +15,7 @@ import { UserService } from '../../../shared/services/user/user.service';
 export class ProfileComponent implements OnInit {
   currentUser: IUser;
   accountInfoForm: FormGroup;
+  isVerified = false;
 
   constructor(
     private userService: UserService,
@@ -38,10 +39,38 @@ export class ProfileComponent implements OnInit {
       name: new FormControl(''),
       phoneNumber: new FormControl(''),
       email: new FormControl(''),
-      streetOne: new FormControl(''),
-      streetTwo: new FormControl(''),
+      streetAddress: new FormControl(''),
+      streetAddress2: new FormControl(''),
       city: new FormControl(''),
       postcode: new FormControl(''),
     });
+
+    setTimeout(() => {
+      this.updateFormValues();
+      console.log('Form values updated');
+    }, 500);
+  }
+
+  updateFormValues() {
+    this.accountInfoForm.setValue({
+      name: this.currentUser.name,
+      phoneNumber: this.currentUser.phoneNumber,
+      email: this.currentUser.email,
+      streetAddress: this.currentUser.streetAddress,
+      streetAddress2: this.currentUser.streetAddress2,
+      city: this.currentUser.city,
+      postcode: this.currentUser.postcode,
+    });
+    console.log(this.accountInfoForm.get('email'));
   }
 }
+
+// this.accountInfoForm = new FormGroup({
+//   name: new FormControl(this.currentUser.name === undefined ? 'this.currentUser.name' : ''),
+//   phoneNumber: new FormControl(this.currentUser.phoneNumber === undefined ? this.currentUser.phoneNumber : ''),
+//   email: new FormControl(this.currentUser.email === undefined ? this.currentUser.email : ''),
+//   streetAddress: new FormControl(this.currentUser.streetAddress === undefined ? this.currentUser.streetAddress : ''),
+//   streetAddress2: new FormControl(this.currentUser.streetAddress2 === undefined ? this.currentUser.streetAddress2 : ''),
+//   city: new FormControl(this.currentUser.city === undefined ? this.currentUser.city : ''),
+//   postcode: new FormControl(this.currentUser.postcode === undefined ? this.currentUser.postcode : ''),
+// });
