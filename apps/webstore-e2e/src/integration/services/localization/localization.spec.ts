f35c953a9ebaba2e666ba5a9fa15e5cee30d8bf5
Kwandes <bz.jasiek@gmail.com>
2021-08-03 01:30:41 +0200

    test: 💍 Write proper e2e test for localization

--- apps/webstore-e2e/src/integration/services/localization/localization.spec.ts
+++ apps/webstore-e2e/src/integration/services/localization/localization.spec.ts
@@ -4,8 +4,17 @@ describe('Localization', () => {
   });
 
   it('do be localized', () => {
-    cy.get('[data-cy=icon]')
-      .invoke('attr', 'alt')
-      .should('contain', 'Language');
+    cy.get('[data-cy=cookie-prompt-modal-accept-cookies-btn]').click();
+
+    cy.get('[data-cy=navbar-localization]').then(() => {
+      cy.get('.language-icon').then(($icon) => {
+        if ($icon.attr('data-cy') === 'dk') {
+          cy.get('[data-cy=dk]');
+          cy.get('[ng-reflect-ng-class=en]');
+        } else {
+          cy.get('[ng-reflect-ng-class=dk]');
+        }
+      });
+    });
   });
 });
