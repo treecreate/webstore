a2dd768d6ebc87a266a9433f88b79cac30d1d346
Kwades <bz.jasiek@gmail.com>
2021-08-24 19:45:37 +0200

    test: 💍 fix broken frontend tests

--- apps/webstore/src/app/pages/auth/profile/profile.component.spec.ts
+++ apps/webstore/src/app/pages/auth/profile/profile.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ProfileComponent } from './profile.component';
 
@@ -10,7 +11,7 @@ describe('ProfileComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ProfileComponent],
-      imports: [HttpClientModule],
+      imports: [RouterTestingModule, HttpClientModule],
     }).compileComponents();
   });
 
