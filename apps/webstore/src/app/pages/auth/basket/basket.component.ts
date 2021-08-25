f901e99aa0ac855f43f6f55fcb727b8e8a5c32dc
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-08-25 12:26:03 +0200

    feat: 🎸 Add no items in basket page

--- apps/webstore/src/app/pages/auth/basket/basket.component.ts
+++ apps/webstore/src/app/pages/auth/basket/basket.component.ts
@@ -9,7 +9,53 @@ import { Component, OnInit } from '@angular/core';
   ],
 })
 export class BasketComponent implements OnInit {
+  basketItems = [
+    {
+      image: 'default',
+      title: 'first design',
+      size: 'small',
+      amount: 2,
+    },
+    {
+      image: 'default',
+      title: 'second design',
+      size: 'large',
+      amount: 1,
+    },
+    {
+      image: 'default',
+      title: 'last design',
+      size: 'medium',
+      amount: 2,
+    },
+  ];
+
+  calcItemPrice(size: string, amount: number) {
+    switch (size) {
+      case 'small':
+        return 495 * amount;
+      case 'medium':
+        return 695 * amount;
+      case 'large':
+        return 995 * amount;
+    }
+  }
+
+  calcFinalPrice(...prices: number[]): number {
+    let sum = 0;
+    for (let i = 0; i < prices.length; i++) {
+      sum += prices[i];
+    }
+    return sum;
+  }
+
+  calcVat(finalPrice: number) {
+    return finalPrice * 0.2;
+  }
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.basketItems);
+  }
 }
