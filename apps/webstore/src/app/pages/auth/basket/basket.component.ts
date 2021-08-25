c0c7cb4fdd705cf953a3c90a947614542888cdd6
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-08-25 14:19:10 +0200

    feat: 🎸 Create basket-item and style it

--- apps/webstore/src/app/pages/auth/basket/basket.component.ts
+++ apps/webstore/src/app/pages/auth/basket/basket.component.ts
@@ -11,35 +11,21 @@ import { Component, OnInit } from '@angular/core';
 export class BasketComponent implements OnInit {
   basketItems = [
     {
-      image: 'default',
-      title: 'first design',
+      title: 'First design',
       size: 'small',
       amount: 2,
     },
     {
-      image: 'default',
-      title: 'second design',
+      title: 'Second design',
       size: 'large',
       amount: 1,
     },
     {
-      image: 'default',
-      title: 'last design',
+      title: 'Last design',
       size: 'medium',
       amount: 2,
     },
   ];
-
-  calcItemPrice(size: string, amount: number) {
-    switch (size) {
-      case 'small':
-        return 495 * amount;
-      case 'medium':
-        return 695 * amount;
-      case 'large':
-        return 995 * amount;
-    }
-  }
 
   calcFinalPrice(...prices: number[]): number {
     let sum = 0;
--- apps/webstore/src/app/shared/components/basket-item/basket-item.component.ts
+++ apps/webstore/src/app/shared/components/basket-item/basket-item.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'webstore-basket-item',
   templateUrl: './basket-item.component.html',
-  styleUrls: ['./basket-item.component.css'],
+  styleUrls: [
+    './basket-item.component.css',
+    '../../../../assets/styles/tc-input-field.scss',
+  ],
 })
 export class BasketItemComponent implements OnInit {
+  @Input() item;
+
   constructor() {}
+
+  calcPrice(size: string, amount: number) {
+    switch (size) {
+      case 'small':
+        return 495 * amount;
+      case 'medium':
+        return 695 * amount;
+      case 'large':
+        return 995 * amount;
+    }
+  }
 
   ngOnInit(): void {}
 }
