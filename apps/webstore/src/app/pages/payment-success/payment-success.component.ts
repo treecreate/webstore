07113e3226b4bcf2ddaebfad49d305d10bf29dbf
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-08-26 11:34:05 +0200

    feat: 🎸 Add extra styling to paymentSuccess page

--- apps/webstore/src/app/pages/payment-success/payment-success.component.ts
+++ apps/webstore/src/app/pages/payment-success/payment-success.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'webstore-payment-success',
   templateUrl: './payment-success.component.html',
-  styleUrls: ['./payment-success.component.css'],
+  styleUrls: [
+    './payment-success.component.css',
+    './payment-success.component.sass',
+  ],
 })
 export class PaymentSuccessComponent implements OnInit {
   constructor() {}
