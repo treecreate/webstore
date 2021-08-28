410824dd63014afc22bfba3fa3bdfc68a5ae293e
Kwandes <bz.jasiek@gmail.com>
2021-08-28 16:01:55 +0200

    feat: 🎸 Add family tree design component

---
+++ apps/webstore/src/app/shared/components/products/family-tree-design/family-tree-design.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FamilyTreeDesignComponent } from './family-tree-design.component';
+
+describe('FamilyTreeDesignComponent', () => {
+  let component: FamilyTreeDesignComponent;
+  let fixture: ComponentFixture<FamilyTreeDesignComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [FamilyTreeDesignComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FamilyTreeDesignComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ apps/webstore/src/app/shared/components/products/family-tree-design/family-tree-design.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'webstore-family-tree-design',
+  templateUrl: './family-tree-design.component.html',
+  styleUrls: [
+    './family-tree-design.component.scss',
+    './family-tree-design.component.mobile.scss',
+    '../../../../../assets/styles/tc-input-field.scss',
+  ],
+})
+export class FamilyTreeDesignComponent implements OnInit {
+  constructor() {}
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
+  ngOnInit(): void {}
+}
