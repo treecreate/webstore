0790fb6ee3d5952ef993cc5030ba9861cb82b247
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-09-02 15:55:06 +0200

    test: 💍 Add tests for addToBasketModal

--- apps/webstore/src/app/shared/components/modals/add-to-basket-modal/add-to-basket-modal.component.spec.ts
+++ apps/webstore/src/app/shared/components/modals/add-to-basket-modal/add-to-basket-modal.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -24,5 +25,71 @@ describe('AddToBasketModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should increase the size', () => {
+    expect(component.addToBasketForm.get('size').value).toEqual('20cm x 20cm');
+    const increaseSize = fixture.debugElement.query(
+      By.css('#increase-size-btn')
+    );
+    const increaseSizeButton = increaseSize.nativeElement;
+    increaseSizeButton.click();
+    expect(component.addToBasketForm.get('size').value).toEqual('25cm x 25cm');
+    increaseSizeButton.click();
+    expect(component.addToBasketForm.get('size').value).toEqual('30cm x 30cm');
+    increaseSizeButton.click();
+    expect(component.addToBasketForm.get('size').value).toEqual('30cm x 30cm');
+  });
+
+  it('should decrease the size', () => {
+    component.addToBasketForm.setValue({
+      size: '30cm x 30cm',
+      amount: 1,
+      title: '',
+    });
+    expect(component.addToBasketForm.get('size').value).toEqual('30cm x 30cm');
+    const decreaseSize = fixture.debugElement.query(
+      By.css('#decrease-size-btn')
+    );
+    const decreaseSizeButton = decreaseSize.nativeElement;
+    decreaseSizeButton.click();
+    expect(component.addToBasketForm.get('size').value).toEqual('25cm x 25cm');
+    decreaseSizeButton.click();
+    expect(component.addToBasketForm.get('size').value).toEqual('20cm x 20cm');
+    decreaseSizeButton.click();
+    expect(component.addToBasketForm.get('size').value).toEqual('20cm x 20cm');
+  });
+
+  it('should increase amount', () => {
+    expect(component.addToBasketForm.get('amount').value).toEqual(1);
+    const increaseAmount = fixture.debugElement.query(
+      By.css('#increase-amount-btn')
+    );
+    const increaseAmountButton = increaseAmount.nativeElement;
+    increaseAmountButton.click();
+    expect(component.addToBasketForm.get('amount').value).toEqual(2);
+    increaseAmountButton.click();
+    expect(component.addToBasketForm.get('amount').value).toEqual(3);
+    increaseAmountButton.click();
+    expect(component.addToBasketForm.get('amount').value).toEqual(4);
+  });
+
+  it('should decrease amount', () => {
+    component.addToBasketForm.setValue({
+      size: '20cm x 20cm',
+      amount: 3,
+      title: '',
+    });
+    expect(component.addToBasketForm.get('amount').value).toEqual(3);
+    const decreaseAmount = fixture.debugElement.query(
+      By.css('#decrease-amount-btn')
+    );
+    const decreaseAmountButton = decreaseAmount.nativeElement;
+    decreaseAmountButton.click();
+    expect(component.addToBasketForm.get('amount').value).toEqual(2);
+    decreaseAmountButton.click();
+    expect(component.addToBasketForm.get('amount').value).toEqual(1);
+    decreaseAmountButton.click();
+    expect(component.addToBasketForm.get('amount').value).toEqual(1);
   });
 });
