8dcce8c3b412e11a17c58bafc38bff2798003ba7
Kwandes <bz.jasiek@gmail.com>
2021-09-07 21:06:30 +0200

    feat: 🎸 Add newsletter functionality to landing page

--- apps/webstore/src/app/pages/landingpage/landingpage.component.ts
+++ apps/webstore/src/app/pages/landingpage/landingpage.component.ts
@@ -1,12 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { INewsletter } from '@interfaces';
+import { ToastService } from '../../shared/components/toast/toast-service';
+import { NewsletterService } from '../../shared/services/newsletter/newsletter.service';
 
 @Component({
   selector: 'webstore-landingpage',
   templateUrl: './landingpage.component.html',
-  styleUrls: ['./landingpage.component.css'],
+  styleUrls: [
+    './landingpage.component.css',
+    '../../../assets/styles/tc-input-field.scss',
+  ],
 })
 export class LandingpageComponent implements OnInit {
-  constructor() {}
+  isNewsletterSignupLoading = false;
 
-  ngOnInit(): void {}
+  newsletterForm: FormGroup;
+
+  constructor(
+    private newsletterService: NewsletterService,
+    private toastService: ToastService
+  ) {}
+
+  ngOnInit(): void {
+    this.newsletterForm = new FormGroup({
+      newsletterEmail: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+    });
+  }
+
+  onNewsletterSignup(): void {
+    this.isNewsletterSignupLoading = true;
+
+    this.newsletterService
+      .registerNewsletterEmail(this.newsletterForm.get('newsletterEmail').value)
+      .subscribe(
+        (data: INewsletter) => {
+          // TODO: make a danish version
+          this.toastService.showAlert(
+            `You've been successfully signed up as ${data.email}`,
+            `No sproge dansk,${data.email}`,
+            'success',
+            2500
+          );
+
+          this.isNewsletterSignupLoading = false;
+        },
+        (error) => {
+          // TODO: translate API errors to danish
+          this.toastService.showAlert(
+            error.error.message,
+            error.error.message,
+            'danger',
+            100000
+          );
+          console.error(error);
+          this.isNewsletterSignupLoading = false;
+        }
+      );
+  }
+
+  isValidEmail(): boolean {
+    return this.newsletterForm.get('newsletterEmail').invalid;
+  }
 }
