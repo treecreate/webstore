709a56ae197625ebae27bf7de84fe7dfd7bf042a
Kwades <bz.jasiek@gmail.com>
2021-09-10 07:46:33 +0200

    test: 💍 Add e2e test for login button in navbar

--- apps/webstore-e2e/src/integration/components/navbar.spec.ts
+++ apps/webstore-e2e/src/integration/components/navbar.spec.ts
@@ -1,5 +1,8 @@
 import { CookieStatus, LocalStorageVars } from '@models';
+import { AuthenticationService, AuthUserEnum } from '@webstore/mocks';
 describe('NavbarComponent', () => {
+  const authMockService = new AuthenticationService();
+
   beforeEach(() => {
     localStorage.setItem(
       LocalStorageVars.cookiesAccepted,
@@ -19,5 +22,16 @@ describe('NavbarComponent', () => {
     cy.get('[data-cy=navbar]').contains('Home').should('exist');
     cy.get('[data-cy=navbar]').contains('Product').should('exist');
     cy.get('[data-cy=navbar]').contains('Log In').should('exist');
+    cy.get('[data-cy=navbar]').contains('Profile').should('not.exist');
+  });
+
+  it('should display profile instead of log in when user is authenticated', () => {
+    localStorage.setItem(
+      LocalStorageVars.authUser,
+      JSON.stringify(authMockService.getMockUser(AuthUserEnum.authUser))
+    );
+
+    cy.get('[data-cy=navbar]').contains('Log In').should('not.exist');
+    cy.get('[data-cy=navbar]').contains('Profile').should('exist');
   });
 });
