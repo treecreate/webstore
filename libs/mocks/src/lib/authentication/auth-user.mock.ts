053ee4b6144baa28d3de58b93f1a9adcc7e963ad
Kwades <bz.jasiek@gmail.com>
2021-09-10 06:21:18 +0200

    feat: 🎸 Add library with mocks of authUser data

---
+++ libs/mocks/src/lib/authentication/auth-user.mock.ts
@@ -0,0 +1,55 @@
+import { IAuthUser } from '@interfaces';
+
+export const authUser: IAuthUser = {
+  email: 'e2e@test.com',
+  accessToken: '',
+  roles: ['ROLE_USER'],
+  tokenType: 'Bearer',
+  userId: '7f000001-7b0d-19bf-817b-0d0a8ec40000',
+  isVerified: true,
+};
+
+export const authUserRoleDeveloper: IAuthUser = {
+  email: 'e2e-dev@test.com',
+  accessToken: '',
+  roles: ['ROLE_USER', 'ROLE_DEVELOPER'],
+  tokenType: 'Bearer',
+  userId: '7f000001-7b0d-19bf-817b-0d0a8ec40000',
+  isVerified: true,
+};
+
+export const authUserRoleOwner: IAuthUser = {
+  email: 'e2e-owner@test.com',
+  accessToken: '',
+  roles: ['ROLE_USER', 'ROLE_OWNER'],
+  tokenType: 'Bearer',
+  userId: '7f000001-7b0d-19bf-817b-0d0a8ec40000',
+  isVerified: true,
+};
+
+export const authUserExpired: IAuthUser = {
+  email: 'e2e-expired@test.com',
+  accessToken: '',
+  roles: ['ROLE_USER'],
+  tokenType: 'Bearer',
+  userId: '7f000001-7b0d-19bf-817b-0d0a8ec40000',
+  isVerified: true,
+};
+
+export const authUserInvalid: IAuthUser = {
+  email: 'e2e-invalid@test.com',
+  accessToken: '',
+  roles: ['ROLE_USER', 'ROLE_DEVELOPER'],
+  tokenType: 'Bearer',
+  userId: '7f000001-7b0d-19bf-817b-0d0a8ec40000',
+  isVerified: true,
+};
+
+export const authUserNotVerified: IAuthUser = {
+  email: 'e2e-not-verified@test.com',
+  accessToken: '',
+  roles: ['ROLE_USER', 'ROLE_DEVELOPER'],
+  tokenType: 'Bearer',
+  userId: '7f000001-7b0d-19bf-817b-0d0a8ec40000',
+  isVerified: false,
+};
