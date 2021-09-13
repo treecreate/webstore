31e2b317c3e969bf09a321dea09a1f517a5d7cde
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-09-13 10:23:51 +0200

    feat: 🎸 Update navbar and global styling

--- apps/webstore/src/app/pages/auth/signup/signup.component.ts
+++ apps/webstore/src/app/pages/auth/signup/signup.component.ts
@@ -78,7 +78,7 @@ export class SignupComponent implements OnInit {
             err.error.message,
             // this was removed to prompt the user to why it wasnt working.
             // 'Signup failed, please try again.',
-            'Der skete en fejl, prøv venligst igen',
+            err.error.message,
             'danger',
             5000
           );
