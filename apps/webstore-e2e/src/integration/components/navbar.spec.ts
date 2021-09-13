d203e33c5015686de8ab88ec63666f636c585f37
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-09-13 15:19:25 +0200

    fix: 🐛 Update failing e2e tests

--- apps/webstore-e2e/src/integration/components/navbar.spec.ts
+++ apps/webstore-e2e/src/integration/components/navbar.spec.ts
@@ -12,12 +12,17 @@ describe('NavbarComponent', () => {
     cy.get('[data-cy=navbar]').should('exist');
   });
 
-  it('should contain a Treecreate logo, Home button, Product button, Log In button, and Basket', () => {
+  it('should contain a Treecreate logo, Product button, Log In button, and Basket', () => {
     cy.get('[data-cy=navbar]')
       .get('[data-cy=navbar-logo-img]')
       .should('be.visible');
-    cy.get('[data-cy=navbar]').contains('Home').should('exist');
+    cy.get('[data-cy=navbar]')
+      .get('[data-cy=navbar-localization]')
+      .should('be.visible');
+    cy.get('[data-cy=navbar]')
+      .get('[data-cy=navbar-basket-link]')
+      .should('be.visible');
     cy.get('[data-cy=navbar]').contains('Product').should('exist');
-    cy.get('[data-cy=navbar]').contains('Log In').should('exist');
+    cy.get('[data-cy=navbar]').contains('Log in').should('exist');
   });
 });
