d4442f50603a65b7ff346f21d82e0551efa962ab
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-09-14 12:52:34 +0200

    test: 💍 Start profile tests and add subscribing service to home

--- apps/webstore-e2e/src/integration/pages/profile/profile.spec.ts
+++ apps/webstore-e2e/src/integration/pages/profile/profile.spec.ts
@@ -1,10 +1,87 @@
-import { CookieStatus, LocalStorageVars } from '@models';
+import { IUser } from '@interfaces';
+import { CookieStatus, LocalStorageVars, UserRoles } from '@models';
+import { AuthenticationService, AuthUserEnum } from '@webstore/mocks';
+
 describe('ProfilePage', () => {
+  const authMockService = new AuthenticationService();
+
+  const mockUser: IUser = {
+    userId: '1',
+    email: 'e2e@test.com',
+    roles: [UserRoles.user],
+    isVerified: false,
+    name: '',
+    phoneNumber: '',
+    streetAddress: '',
+    streetAddress2: '',
+    city: '',
+    postcode: '',
+    country: '',
+  };
+
+  const updatedMockUser: IUser = {
+    userId: '1',
+    email: 'e2e@test.com',
+    roles: [UserRoles.user],
+    isVerified: false,
+    name: '',
+    phoneNumber: '',
+    streetAddress: '',
+    streetAddress2: '',
+    city: '',
+    postcode: '',
+    country: '',
+  };
+
   beforeEach(() => {
     localStorage.setItem(
       LocalStorageVars.cookiesAccepted,
       `"${CookieStatus.accepted}"` // localStorage saves the data differently from our LocalStorageService
     );
+    localStorage.setItem(
+      LocalStorageVars.authUser,
+      JSON.stringify(
+        authMockService.getMockUser(AuthUserEnum.authUserNotVerified)
+      )
+    );
+    cy.intercept('POST', `/auth/signin`, {
+      body: authMockService.getMockUser(AuthUserEnum.authUser),
+      statusCode: 200,
+    }).as('signinRequest');
     cy.visit('/login');
+    cy.get('[data-cy=login-email-input]').type('e2e@test.com');
+    cy.get('[data-cy=login-password-input]').type('abcDEF123');
+    cy.get('[data-cy=login-btn]').click();
+  });
+
+  it('should update user', () => {
+    //Mock return user request
+    cy.intercept('GET', '/users/me', {
+      body: mockUser,
+      statusCode: 200,
+    });
+    //Mock update user request
+    cy.intercept('PUT', '/users', {
+      body: updatedMockUser,
+      statusCode: 200,
+    });
+
+    //Update values in form to mach the retrieved user
+
+    cy.get('[data-cy=profile-email-input]');
+
+    //Change values in form
+    cy.get('[data-cy=profile-name-input]').type('firstName secondName');
+    cy.get('[data-cy=profile-phone-number-input]').type('+4512345678');
+    cy.get('[data-cy=profile-street-address-input]').type(
+      'Yo mammas house 69, 3rd floor'
+    );
+    cy.get('[data-cy=profile-street-address-2-input]').type(
+      'more details: suck it'
+    );
+    cy.get('[data-cy=profile-city-input]').type('Copenhagen');
+    cy.get('[data-cy=profile-postcode-input]').type('1234');
+
+    //Update user request
   });
 });
--- apps/webstore/src/app/pages/home/home.component.ts
+++ apps/webstore/src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { IAuthUser } from '@interfaces';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IAuthUser, INewsletter } from '@interfaces';
 import { LocalStorageVars } from '@models';
 import { BehaviorSubject } from 'rxjs';
+import { ToastService } from '../../shared/components/toast/toast-service';
 import { AuthService } from '../../shared/services/authentication/auth.service';
 import { LocalStorageService } from '../../shared/services/local-storage';
+import { NewsletterService } from '../../shared/services/newsletter/newsletter.service';
 @Component({
   selector: 'webstore-home',
   templateUrl: './home.component.html',
@@ -15,12 +18,16 @@ export class HomeComponent implements OnInit {
   showStartButton = false;
   public isLoggedIn: boolean;
   private authUser$: BehaviorSubject<IAuthUser>;
+  subscribeForm: FormGroup;
+  isSubscribingUser = false;
 
   title = 'homeComponent';
 
   constructor(
     private localStorageService: LocalStorageService,
-    private authService: AuthService
+    private authService: AuthService,
+    private newsletterService: NewsletterService,
+    private toastService: ToastService
   ) {
     this.initialTop = 0;
 
@@ -35,6 +42,38 @@ export class HomeComponent implements OnInit {
         this.authUser$.getValue() != null &&
         this.authService.isAccessTokenValid();
     });
+
+    this.subscribeForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  submitNewsletterEmail() {
+    this.isSubscribingUser = true;
+    this.newsletterService
+      .registerNewsletterEmail(this.subscribeForm.get('email').value)
+      .subscribe(
+        (data: INewsletter) => {
+          this.toastService.showAlert(
+            `Thank you for subscribing: ${data.email}`,
+            `Tak for din tilmelding: ${data.email}`,
+            'success',
+            3000
+          );
+          this.isSubscribingUser = false;
+        },
+        (error) => {
+          // TODO: translate API errors to danish
+          this.toastService.showAlert(
+            error.error.message,
+            error.error.message,
+            'danger',
+            100000
+          );
+          console.error(error);
+          this.isSubscribingUser = false;
+        }
+      );
   }
 
   @HostListener('window:scroll')
