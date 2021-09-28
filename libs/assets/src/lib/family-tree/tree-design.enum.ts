65c520c5d09ad6540436a7ccfccf88bd9dfa0b98
Kwandes <bz.jasiek@gmail.com>
2021-09-28 21:07:11 +0200

    feat: 🎸 Add more name values to tree design name enum

--- libs/assets/src/lib/family-tree/tree-design.enum.ts
+++ libs/assets/src/lib/family-tree/tree-design.enum.ts
@@ -1,9 +1,11 @@
 export enum TreeDesignEnum {
   tree1 = 'assets/family-tree/tree-design/tree1.svg',
   tree2 = 'assets/family-tree/tree-design/tree2.svg',
+  tree3 = 'assets/family-tree/tree-design/tree3.svg',
 }
 
 export enum TreeDesignNameEnum {
   tree1 = 'First',
   tree2 = 'Second',
+  tree3 = 'Third',
 }
