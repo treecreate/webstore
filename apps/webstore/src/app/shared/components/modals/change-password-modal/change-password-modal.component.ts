bf5330fb417de87bcbec1fbdebfede91de42b37c
Kwades <bz.jasiek@gmail.com>
2021-09-29 19:03:58 +0200

    refactor: 💡 Change regex for password validation in reset pass

--- apps/webstore/src/app/shared/components/modals/change-password-modal/change-password-modal.component.ts
+++ apps/webstore/src/app/shared/components/modals/change-password-modal/change-password-modal.component.ts
@@ -30,12 +30,12 @@ export class ChangePasswordModalComponent implements OnInit {
     this.changePasswordForm = new FormGroup({
       password: new FormControl('', [
         Validators.required,
-        Validators.minLength(6),
+        Validators.minLength(8),
         Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$'),
       ]),
       confirmPassword: new FormControl('', [
         Validators.required,
-        Validators.minLength(6),
+        Validators.minLength(8),
         Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$'),
       ]),
     });
