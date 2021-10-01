d49a7bfe21e06f3dcfef0fbd9d209f9aab7e36f9
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-10-01 19:11:06 +0200

    test: 💍 e2e tests for resetPassword page

---
+++ apps/webstore-e2e/src/integration/pages/reset-password/reset-password.spec.ts
@@ -0,0 +1,54 @@
+import { LocalStorageVars, CookieStatus } from '@models';
+
+const resetPasswordUUID = '1c1ca614-6600-4e61-aec5-8e6143b1';
+describe('resetPasswordPage', () => {
+  beforeEach(() => {
+    localStorage.setItem(
+      LocalStorageVars.cookiesAccepted,
+      `"${CookieStatus.accepted}"` // localStorage saves the data differently from our LocalStorageService
+    );
+    cy.visit('/resetPassword/' + resetPasswordUUID);
+  });
+
+  it('should show error messages when password is not good enough', () => {
+    cy.get('[data-cy=reset-password-password-input]').type('abcDEFabcDEF');
+    cy.get('[data-cy=reset-password-password-error-message]').should('exist');
+    cy.get('[data-cy=reset-password-password-input]').type('123');
+    cy.get('[data-cy=reset-password-password-error-message]').should(
+      'not.exist'
+    );
+    cy.get('[data-cy=reset-password-password-input]').clear();
+    cy.get('[data-cy=reset-password-password-input]').type('abCD12');
+    cy.get('[data-cy=reset-password-password-error-message]').should('exist');
+  });
+
+  it('should display confirm password error message when it doesnt match password', () => {
+    cy.get('[data-cy=reset-password-password-input]').type('abcDEF123');
+    cy.get('[data-cy=reset-password-confirm-password-input]').type('abcDEF321');
+    cy.get('[data-cy=reset-password-confirm-password-error-message]').should(
+      'exist'
+    );
+    cy.get('[data-cy=reset-password-confirm-password-input]').clear();
+    cy.get('[data-cy=reset-password-confirm-password-input]').type('abcDEF123');
+    cy.get('[data-cy=reset-password-confirm-password-error-message]').should(
+      'not.exist'
+    );
+  });
+
+  it('should disable reset password button when passwords dont match', () => {
+    cy.get('[data-cy=reset-password-button]').should('be.disabled');
+    cy.get('[data-cy=reset-password-password-input]').type('abcDEF123');
+    cy.get('[data-cy=reset-password-button]').should('be.disabled');
+    cy.get('[data-cy=reset-password-confirm-password-input]').type('abcDEF123');
+    cy.get('[data-cy=reset-password-button]').should('not.be.disabled');
+    cy.get('[data-cy=reset-password-confirm-password-input]').type('4');
+    cy.get('[data-cy=reset-password-button]').should('be.disabled');
+  });
+
+  it('should display failed message after changing password', () => {
+    cy.get('[data-cy=reset-password-password-input]').type('abcDEF123');
+    cy.get('[data-cy=reset-password-confirm-password-input]').type('abcDEF123');
+    cy.get('[data-cy=reset-password-button]').click();
+    cy.get('[data-cy=reset-password-failed]').should('exist');
+  });
+});
