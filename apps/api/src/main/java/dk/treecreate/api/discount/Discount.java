45eeabbf264bc80f9c42274a5437013f935ebabd
Kwandes <bz.jasiek@gmail.com>
2021-10-04 22:14:44 +0200

    feat: 🎸 Create a discount entity

---
+++ apps/api/src/main/java/dk/treecreate/api/discount/Discount.java
@@ -0,0 +1,148 @@
+package dk.treecreate.api.discount;
+
+import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "discounts",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = "discount_code"),
+    })
+public class Discount
+{
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+        name = "UUID",
+        strategy = "org.hibernate.id.UUIDGenerator",
+        parameters = {
+            @org.hibernate.annotations.Parameter(
+                name = "uuid_gen_strategy_class",
+                value = "org.hibernate.id.uuid.CustomVersionOneStrategy"
+            )
+        }
+    )
+    @Type(type = "uuid-char")
+    @Column(name = "discount_id", updatable = false, nullable = false)
+    @ApiModelProperty(notes = "UUID of the user entity",
+        example = "c0a80121-7ac0-190b-817a-c08ab0a12345")
+    private UUID discountId;
+
+
+    @Basic
+    @ApiModelProperty(example = "DiscountRelease2021", required = true)
+    @Column(name = "discount_code", length = 50, nullable = false)
+    private String discountCode;
+
+    @Basic
+    @Min(0)
+    @ApiModelProperty(name = "How many more times the discount can be used", example = "0",
+        required = true)
+    @Column(name = "remaining_uses", nullable = false)
+    private int remainingUses = 0;
+
+    @Basic
+    @Min(0)
+    @ApiModelProperty(name = "How many times the discount has been used", example = "1",
+        required = true)
+    @Column(name = "total_uses", nullable = false)
+    private int totalUses = 0;
+
+    @ApiModelProperty(name = "Date after which the discount can no longer be used",
+        example = "2021-08-31T19:40:10.000+00:00", required = false)
+    @Column(name = "expires_at", nullable = true)
+    private Date expiresAt;
+
+    @ApiModelProperty(name = "Date the discount entry was created at",
+        example = "2021-08-31T19:40:10.000+00:00")
+    @CreationTimestamp
+    private Date createdAt;
+
+    public UUID getDiscountId()
+    {
+        return discountId;
+    }
+
+    public void setDiscountId(UUID discountId)
+    {
+        this.discountId = discountId;
+    }
+
+    public String getDiscountCode()
+    {
+        return discountCode;
+    }
+
+    public void setDiscountCode(String discountCode)
+    {
+        this.discountCode = discountCode;
+    }
+
+    public int getRemainingUses()
+    {
+        return remainingUses;
+    }
+
+    public void setRemainingUses(int remainingUses)
+    {
+        this.remainingUses = remainingUses;
+    }
+
+    public int getTotalUses()
+    {
+        return totalUses;
+    }
+
+    public void setTotalUses(int totalUses)
+    {
+        this.totalUses = totalUses;
+    }
+
+    public Date getExpiresAt()
+    {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(Date expiresAt)
+    {
+        this.expiresAt = expiresAt;
+    }
+
+    public Date getCreatedAt()
+    {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt)
+    {
+        this.createdAt = createdAt;
+    }
+
+    @Override public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Discount discount = (Discount) o;
+        return remainingUses == discount.remainingUses && totalUses == discount.totalUses &&
+            discountId.equals(discount.discountId) && discountCode.equals(discount.discountCode) &&
+            Objects.equals(expiresAt, discount.expiresAt) &&
+            Objects.equals(createdAt, discount.createdAt);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(discountId, discountCode, remainingUses, totalUses, expiresAt,
+            createdAt);
+    }
+
+    // TODO - OneToMany relationship to orders
+}
