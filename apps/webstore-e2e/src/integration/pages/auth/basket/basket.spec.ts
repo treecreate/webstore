22be46978feecc41b868b936c2f44505300fc44e
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-10-06 12:26:23 +0200

    test: 💍 Write what tests should be in basket

--- apps/webstore-e2e/src/integration/pages/auth/basket/basket.spec.ts
+++ apps/webstore-e2e/src/integration/pages/auth/basket/basket.spec.ts
@@ -16,5 +16,53 @@ describe('BasketPage', () => {
         );
     }); 
 
-    
+    it('should increase amount of trees donated', () => {
+
+    });
+
+    it('should decrease amount of trees donated', () => {
+
+    }); 
+
+    it('should apply discount', () => {
+
+    }); 
+
+    it('should go to checkout', () => {
+
+    }); 
+
+    describe('AddToBasketModal', () => {
+        it('should save design and direct to login if user is not logged in', () => {
+
+        });
+
+        it('should have the title written if it is in the product page', () => {
+            
+        });
+
+        it('should be able to change title', () => {
+            
+        });
+
+        it('it should set the title to untitled if there is no title', () => {
+            
+        });
+
+        it('should increase quantity', () => {
+            
+        });
+
+        it('should decrease quantity', () => {
+            
+        });
+
+        it('should increase dimensions', () => {
+            
+        });
+
+        it('should decrease dimensions', () => {
+            
+        });
+    }); 
 })
