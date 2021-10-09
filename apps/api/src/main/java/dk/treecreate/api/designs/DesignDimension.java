435d6bc8661df256c60a0f4d89a1f23bb02a7410
Kwandes <bz.jasiek@gmail.com>
2021-10-09 19:46:47 +0200

    refactor: 💡 Add labels to Design enums

--- apps/api/src/main/java/dk/treecreate/api/designs/DesignDimension.java
+++ apps/api/src/main/java/dk/treecreate/api/designs/DesignDimension.java
@@ -4,8 +4,15 @@ public enum DesignDimension
 {
     // if the order of these ever gets changed, the saved transaction items have to be updated since the entry uses the enum index (0 = family tree etc)
     // If you don't update the saved designs, they will be returning a wrong (old) design dimensions index
-    SMALL,
-    MEDIUM,
-    LARGE,
-    ONE_SIZE;
+    SMALL("SMALL"),
+    MEDIUM("MEDIUM"),
+    LARGE("LARGE"),
+    ONE_SIZE("ONE_SIZE");
+
+    public final String label;
+
+    DesignDimension(String label)
+    {
+        this.label = label;
+    }
 }
