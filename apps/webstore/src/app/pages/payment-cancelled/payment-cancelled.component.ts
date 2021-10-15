7b376f75f896177149e24c39544fb35bc3d607e9
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-10-15 15:36:42 +0200

    feat: 🎸 Create payment cancelled page

---
+++ apps/webstore/src/app/pages/payment-cancelled/payment-cancelled.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'webstore-payment-cancelled',
+  templateUrl: './payment-cancelled.component.html',
+  styleUrls: ['./payment-cancelled.component.css'],
+})
+export class PaymentCancelledComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
