17344f372d9731eacb00a79bf77506803515ecbe
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-10-17 09:07:47 +0200

    refactor: 💡 Remove duplicate getItemsInBasket methods

--- apps/webstore/src/app/shared/components/navbar/navbar.component.ts
+++ apps/webstore/src/app/shared/components/navbar/navbar.component.ts
@@ -81,7 +81,6 @@ export class NavbarComponent implements OnInit {
       LocalStorageVars.locale,
       this.localeCode
     );
-    this.getItemsInBasket();
   }
 
   getItemsInBasket() {
@@ -189,18 +188,6 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.transactionItemService.getTransactionItems().subscribe(
-      (itemList: ITransactionItem[]) => {
-        this.itemList = itemList;
-        let sum = 0;
-        for (let i = 0; i < itemList.length; i++) {
-          sum += itemList[i].quantity;
-        }
-        this.itemsInBasket = sum;
-      },
-      (error: HttpErrorResponse) => {
-        console.error(error);
-      }
-    );
+    this.getItemsInBasket(); 
   }
 }
