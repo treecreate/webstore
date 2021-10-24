e0727e52b12c546ba58a97ba6b2ff7ef1f82b9f2
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-10-24 22:42:00 +0200

    fix: 🐛 Update failing CI test

--- apps/webstore/src/app/shared/components/modals/go-to-basket-modal/go-to-basket-modal.component.spec.ts
+++ apps/webstore/src/app/shared/components/modals/go-to-basket-modal/go-to-basket-modal.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { GoToBasketModalComponent } from './go-to-basket-modal.component';
@@ -10,8 +12,8 @@ describe('GoToBasketModalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [GoToBasketModalComponent],
-      imports: [NgbModule],
       providers: [NgbActiveModal],
+      imports: [NgbModule, RouterTestingModule, HttpClientModule],
     }).compileComponents();
   });
 
