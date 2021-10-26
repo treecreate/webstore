871298618579e7eb709adb22a6ee38905e4d1c09
Kwades <bz.jasiek@gmail.com>
2021-10-26 20:58:23 +0200

    feat: 🎸 Add automatic change of locale to match local storage
    Only affects deployed version of the page

--- apps/webstore/src/app/app.component.ts
+++ apps/webstore/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { LocaleType, LocalStorageVars } from '@models';
 import { environment } from '../environments/environment';
+import { LocalStorageService } from './shared/services/local-storage';
 
 // Google analytics-specific syntax
 // eslint-disable-next-line @typescript-eslint/ban-types
@@ -12,7 +14,12 @@ declare let gtag: Function;
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(public router: Router) {
+  title = 'Treecreate';
+
+  constructor(
+    public router: Router,
+    private localStorageService: LocalStorageService
+  ) {
     //Google analytics
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
@@ -28,7 +35,23 @@ export class AppComponent {
         }
       }
     });
-  }
 
-  title = 'Treecreate';
+    this.router.events.subscribe(() => {
+      const locale = this.localStorageService
+        .getItem<LocaleType>(LocalStorageVars.locale)
+        .getValue();
+      // if the website is deployed the url has locale in it and has to be adjusted to match local storage
+      if (
+        window.location.href.includes('/en-US/') &&
+        locale === LocaleType.dk
+      ) {
+        window.location.href = window.location.href.replace('en-US', 'dk');
+      } else if (
+        window.location.href.includes('/dk/') &&
+        locale === LocaleType.en
+      ) {
+        window.location.href = window.location.href.replace('dk', 'en-US');
+      }
+    });
+  }
 }
