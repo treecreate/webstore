159003dd8fd93e7552f65768f90ca38d4492d1b3
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-11-08 18:14:16 +0100

    style: 💄 Apply formatting

--- apps/webstore/src/app/pages/auth/login/login.component.ts
+++ apps/webstore/src/app/pages/auth/login/login.component.ts
@@ -46,9 +46,7 @@ export class LoginComponent implements OnInit {
 
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [
-        Validators.required,
-      ]),
+      password: new FormControl('', [Validators.required]),
     });
   }
 
