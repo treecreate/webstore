054aa78355d30d8c8268d74e17e43b3826f5108f
Kwades <bz.jasiek@gmail.com>
2021-11-11 19:17:28 +0100

    feat: 🎸 Init ngzorro with `nx g ng-zorro-antd:ng-add`
    Init ng zorro with `nx g ng-zorro-antd:ng-add --project=admin-page`
    Closes: tc-33
    command. Options: Icons yes, theme no, en_US locale and blank layout

--- apps/admin-page/src/app/app.module.ts
+++ apps/admin-page/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
