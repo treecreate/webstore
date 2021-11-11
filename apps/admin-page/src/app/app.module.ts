20c487a3e62216dc8adeddd1f1e0a5164ba80774
Kwades <bz.jasiek@gmail.com>
2021-11-11 19:58:15 +0100

    feat: 🎸 Add example button with ngzorro styling
    ✅ Closes: tc-33

--- apps/admin-page/src/app/app.module.ts
+++ apps/admin-page/src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import en from '@angular/common/locales/en';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n';
+import { AppComponent } from './app.component';
+import { nzModules } from './nz-modules.constant';
 
 registerLocaleData(en);
 
@@ -21,6 +20,7 @@ registerLocaleData(en);
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    nzModules,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
