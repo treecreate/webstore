473022f72cb21b8480e5061527021db37255c187
Cristian <cristi.valentin29@gmail.com>
2021-11-19 07:06:25 +0100

    test: 💍 provide router module for tests

--- apps/admin-page/src/app/components/layout/layout.component.spec.ts
+++ apps/admin-page/src/app/components/layout/layout.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LayoutComponent } from './layout.component';
 
@@ -9,6 +10,7 @@ describe('LayoutComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LayoutComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
   });
 
