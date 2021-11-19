e0877c3e59b02e57200274bfc35f9723e13e7e92
Kwades <bz.jasiek@gmail.com>
2021-11-19 06:51:12 +0100

    docs: TC-17 Add JSDoc for authService methods

--- apps/webstore/src/app/shared/services/authentication/auth.service.ts
+++ apps/webstore/src/app/shared/services/authentication/auth.service.ts
@@ -18,6 +18,11 @@ const httpOptions = {
 export class AuthService {
   constructor(private http: HttpClient, private localStorageService: LocalStorageService, private router: Router) {}
 
+  /**
+   * Perform a login request to the API
+   * @param params user credentials
+   * @returns observable of the API request
+   */
   login(params: ILoginRequestParams): Observable<ILoginResponse> {
     const { email, password } = params;
     return this.http.post<ILoginResponse>(
@@ -30,6 +35,11 @@ export class AuthService {
     );
   }
 
+  /**
+   * Perform a signup/registration request to the API
+   * @param params user credentials
+   * @returns observable of the API request
+   */
   register(params: IRegisterRequestParams): Observable<IRegisterResponse> {
     const { email, password } = params;
     return this.http.post<IRegisterResponse>(
@@ -42,29 +52,41 @@ export class AuthService {
     );
   }
 
-  public logout() {
+  /**
+   * Remove the user authentication information from local storage
+   */
+  public logout(): void {
     this.localStorageService.removeItem(LocalStorageVars.authUser);
     // the tree design should no longer be accessible
     this.localStorageService.removeItem(LocalStorageVars.designFamilyTree);
     this.router.navigate(['/home']);
   }
 
-  // Save auth user information to local storage
+  /**
+   * Save auth user information to local storage
+   * @param user authUser object with information like accessToken
+   */
   public saveAuthUser(user: IAuthUser): void {
     this.localStorageService.removeItem(LocalStorageVars.authUser);
     this.localStorageService.setItem<IAuthUser>(LocalStorageVars.authUser, user);
   }
 
-  // Get user information for authentication. The data comes from local storage. Use getUser() to get full user entity
+  /**
+   * Get user information for authentication. The data comes from local storage. Use getUser() to get full user entity
+   * @returns user information needed for authentication and authorization. Returns null if no information is found
+   */
   public getAuthUser(): IAuthUser | null {
     const user = this.localStorageService.getItem<IAuthUser>(LocalStorageVars.authUser);
     if (user) {
       return user.getValue();
     }
-
     return null;
   }
 
+  /**
+   * Validates that the access token saved in the local storage is valid and not expired
+   * @returns whether the access token is valid and not expired
+   */
   public isAccessTokenValid(): boolean {
     const authUser = this.localStorageService.getItem<IAuthUser>(LocalStorageVars.authUser);
     if (authUser !== null && authUser.getValue() != null) {
@@ -82,6 +104,11 @@ export class AuthService {
     return false;
   }
 
+  /**
+   * Validates if the given JWT is expired
+   * @param token JWT token
+   * @returns whether it is expired or not
+   */
   public isJwtExpired(token: string): boolean {
     const expiry = JSON.parse(atob(token.split('.')[1])).exp;
     return Math.floor(new Date().getTime() / 1000) >= expiry;
