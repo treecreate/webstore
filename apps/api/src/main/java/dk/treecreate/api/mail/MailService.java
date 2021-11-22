8084cdb2d8b392c061853ba5048404c8edc9330f
Kwades <bz.jasiek@gmail.com>
2021-11-22 19:12:31 +0100

    feat: Add javadoc to MailService

--- apps/api/src/main/java/dk/treecreate/api/mail/MailService.java
+++ apps/api/src/main/java/dk/treecreate/api/mail/MailService.java
@@ -38,6 +38,15 @@ public class MailService
         this.orderMailSender = orderMailSender;
     }
 
+    /**
+     * Send a welcome email to new user
+     *
+     * @param to     the target email
+     * @param token  the special id of the user
+     * @param locale language the email should be sent in
+     * @throws UnsupportedEncodingException
+     * @throws MessagingException
+     */
     public void sendSignupEmail(String to, UUID token, Locale locale)
         throws UnsupportedEncodingException, MessagingException
     {
@@ -49,6 +58,15 @@ public class MailService
         sendMail(to, MailDomain.INFO, subject, context, MailTemplate.SIGNUP);
     }
 
+    /**
+     * Send a password reset email
+     *
+     * @param to     the target email
+     * @param token  the special id of the user used to provide a link for resetting the password
+     * @param locale language the email should be sent in
+     * @throws UnsupportedEncodingException
+     * @throws MessagingException
+     */
     public void sendResetPasswordEmail(String to, UUID token, Locale locale)
         throws UnsupportedEncodingException, MessagingException
     {
@@ -71,6 +89,15 @@ public class MailService
         sendMail(to, MailDomain.INFO, subject, context, MailTemplate.VERIFY_EMAIL);
     }
 
+
+    /**
+     * Send an order confirmation email
+     *
+     * @param to    the target email
+     * @param order order object with order information
+     * @throws UnsupportedEncodingException
+     * @throws MessagingException
+     */
     public void sendOrderConfirmationEmail(String to, Order order)
         throws UnsupportedEncodingException, MessagingException
     {
@@ -84,6 +111,15 @@ public class MailService
             MailTemplate.ORDER_CONFIRMATION);
     }
 
+    /**
+     * Send an newsletter email with a discount for new newsletter subscriber
+     *
+     * @param to           the target email
+     * @param newsletterId the id of the new newsletter signup
+     * @param locale       language the email should be sent in
+     * @throws UnsupportedEncodingException
+     * @throws MessagingException
+     */
     public void sendNewNewsletterSubscriberEmail(String to, UUID newsletterId, Locale locale)
         throws UnsupportedEncodingException, MessagingException
     {
@@ -95,6 +131,17 @@ public class MailService
         sendMail(to, MailDomain.INFO, subject, context, MailTemplate.NEW_NEWSLETTER_SUBSCRIBER);
     }
 
+    /**
+     * Send a email based on the params
+     *
+     * @param to       the target email
+     * @param from     domain the email should be sent under
+     * @param subject  email subject
+     * @param context  variables used throughout the email
+     * @param template which email template to use
+     * @throws MessagingException
+     * @throws UnsupportedEncodingException
+     */
     private void sendMail(String to, MailDomain from, String subject, Context context,
                           MailTemplate template)
         throws MessagingException, UnsupportedEncodingException
@@ -112,6 +159,12 @@ public class MailService
         mailSender.send(mimeMessage);
     }
 
+    /**
+     * Is the provided email valid
+     *
+     * @param email email to verify
+     * @return whether it is valid or not
+     */
     public boolean isValidEmail(String email)
     {
         // Java mail does not check for length
@@ -127,6 +180,12 @@ public class MailService
         }
     }
 
+    /**
+     * Get the account domain the email should be sent under
+     *
+     * @param mailDomain which domain to use
+     * @return the domain
+     */
     private JavaMailSender getMailSender(MailDomain mailDomain)
     {
         if (mailDomain == MailDomain.ORDER)
