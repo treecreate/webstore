82a3d4ebd843b4a34e2b9a55e8758db6c7d626d3
Cristian <cristi.valentin29@gmail.com>
2021-11-24 12:55:48 +0100

    feat: TC-39 add sidebar and remove layout component

--- apps/admin-page/src/app/app.component.ts
+++ apps/admin-page/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'admin-page';
+  public sidebarCollapsed: boolean;
+  public username: string;
+  public content: string;
+
+  constructor() {
+    this.sidebarCollapsed = false;
+    this.username = 'username@example.com';
+    this.content = 'content';
+  }
+
+  toggleCollapsed() {
+    this.sidebarCollapsed = !this.sidebarCollapsed;
+  }
 }
--- apps/admin-page/src/app/material.constant.ts
+++ apps/admin-page/src/app/material.constant.ts
@@ -1,5 +1,19 @@
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+import { MatCommonModule } from '@angular/material/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
-export const materialModules = [MatSliderModule, MatToolbarModule, MatButtonModule];
+export const materialModules = [
+  MatCommonModule,
+  MatSliderModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatDividerModule,
+  MatIconModule,
+  MatListModule,
+];
