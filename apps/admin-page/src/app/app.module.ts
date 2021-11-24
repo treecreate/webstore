7c756a5f4fb5d412303f02512961dac48ba3413f
Kwades <bz.jasiek@gmail.com>
2021-11-24 08:19:30 +0100

    refactor: TC-17 Adjust pahge not found to use material UI

--- apps/admin-page/src/app/app.module.ts
+++ apps/admin-page/src/app/app.module.ts
@@ -8,11 +8,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { materialModules } from './material.constant';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PageNotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, BrowserAnimationsModule, materialModules],
   providers: [],
   bootstrap: [AppComponent],
--- apps/admin-page/src/app/material.constant.ts
+++ apps/admin-page/src/app/material.constant.ts
@@ -1,3 +1,4 @@
+import { MatButtonModule } from '@angular/material/button';
 import { MatSliderModule } from '@angular/material/slider';
 
-export const materialModules = [MatSliderModule];
+export const materialModules = [MatSliderModule, MatButtonModule];
