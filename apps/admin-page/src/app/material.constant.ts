bddf96d4ae30ace510fd937a6692a7379daebfab
Cristian <cristi.valentin29@gmail.com>
2021-11-25 12:01:08 +0100

    fix: TC-39 remove unused dependencies

--- apps/admin-page/src/app/material.constant.ts
+++ apps/admin-page/src/app/material.constant.ts
@@ -3,13 +3,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatSliderModule } from '@angular/material/slider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
 export const materialModules = [
   MatCommonModule,
-  MatSliderModule,
   MatToolbarModule,
   MatButtonModule,
   MatSidenavModule,
