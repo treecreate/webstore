cc27f15fd30e4efbbf2553701bf4eaf1cada930a
Cristian <cristi.valentin29@gmail.com>
2021-11-30 11:40:55 +0100

    test: TC-26 add item card tests

---
+++ apps/admin-page-e2e/src/integration/components/item-card.spec.ts
@@ -0,0 +1,33 @@
+import { IUser } from '@interfaces';
+import { LocalStorageVars, UserRoles } from '@models';
+import { AuthenticationService, AuthUserEnum } from '@webstore/mocks';
+
+const authMockService = new AuthenticationService();
+
+describe('item-card', () => {
+  beforeEach(() => {
+    localStorage.setItem(LocalStorageVars.authUser, JSON.stringify(authMockService.getMockUser(AuthUserEnum.authUser)));
+    cy.visit('/orders');
+  });
+
+  it('should contain a card', () => {
+    cy.get('[data-cy=card]').should('exist');
+  });
+
+  it('should contain a visible title', () => {
+    cy.get('[data-cy=card-title]').should('exist');
+    cy.get('[data-cy=card-title]').should('be.visible');
+  });
+
+  it('should contain a "Orders" as a title', () => {
+    cy.get('[data-cy=card-title]').contains('Orders').should('exist');
+  });
+
+  it('should contain a card divider', () => {
+    cy.get('[data-cy=card-divider]').should('exist');
+  });
+
+  it('should contain a card content', () => {
+    cy.get('[data-cy=card-content]').should('exist');
+  });
+});
