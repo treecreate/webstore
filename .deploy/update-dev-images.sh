cbf4f011168caa2242ac3114d15a7e4ce3c57fcd
IroniX2 <57399961+IroniX2@users.noreply.github.com>
2021-12-01 09:30:26 +0100

    ci: TC-34 Change ports defined in compose run and remove ports from buil

--- .deploy/update-dev-images.sh
+++ .deploy/update-dev-images.sh
@@ -5,7 +5,7 @@
 cat .deploy/.env-files/.env.dockerhub | docker login --username kwandes --password-stdin;
 
 # Builds the dev images
-docker-compose -f .deploy/docker-compose-build.yml --profile dev build
+docker-compose -f .deploy/docker-compose-build.yml --profile dev build --parallel
 
 # Publishes dev images
 docker-compose -f .deploy/docker-compose-build.yml --profile dev push
