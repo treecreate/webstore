6744136f2cfc2d0dc0e863918f7cd43da58fbfbf
IroniX2 <57399961+IroniX2@users.noreply.github.com>
2021-12-01 08:09:12 +0100

    ci: TC-34 Add run based docker compse, deploy script, and new ENVs
    total list of ENVs now is [".env.dev.api", ".env.dev.frontend", ".env.prod.api",
    ".env.prod.frontend", ".env.dockerhub"]

---
+++ .deploy/update-dev-images.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# Logs in to docker hub
+cat .deploy/.env-files/.env.dockerhub | docker login --username kwandes --password-stdin;
+
+# Builds the dev images
+docker-compose -f .deploy/docker-compose-build.yml --profile dev build
+
+# Publishes dev images
+docker-compose -f .deploy/docker-compose-build.yml --profile dev push
