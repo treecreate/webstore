ef813e4b5296b284e831511ed8f32765b7d51830
Cristian <cristi.valentin29@gmail.com>
2021-12-07 08:14:34 +0100

    feat: TC-30 add jsdoc

--- apps/admin-page/src/app/services/orders/orders.service.ts
+++ apps/admin-page/src/app/services/orders/orders.service.ts
@@ -10,10 +10,21 @@ import { environment as env } from '../../../environments/environment';
 export class OrdersService {
   constructor(private http: HttpClient) {}
 
+  /**
+   * Calls the API to fetch all available orders.
+   * 
+   * @returns an observable with the list of orders.
+   */
   public getOrders(): Observable<IOrder[]> {
     return this.http.get<IOrder[]>(`${env.apiUrl}/orders`);
   }
 
+  /**
+   * Calls the API to update the status of an order.
+   * 
+   * @param order - the order that needs to be updated.
+   * @returns an observable with the updated order.
+   */
   public updateOrder(order: IOrder): Observable<IOrder> {
     return this.http.patch<IOrder>(`${env.apiUrl}/orders/status/${order.orderId}`, { status: order.status });
   }
