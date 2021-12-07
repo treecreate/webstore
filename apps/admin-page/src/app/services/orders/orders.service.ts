6a07aaeb80544319dbe064daa761b2d696c51a37
Cristian <cristi.valentin29@gmail.com>
2021-12-07 11:37:23 +0100

    refactor: TC-30 format files

--- apps/admin-page/src/app/services/orders/orders.service.ts
+++ apps/admin-page/src/app/services/orders/orders.service.ts
@@ -12,7 +12,7 @@ export class OrdersService {
 
   /**
    * Calls the API to fetch all available orders.
-   * 
+   *
    * @returns an observable with the list of orders.
    */
   public getOrders(): Observable<IOrder[]> {
@@ -21,7 +21,7 @@ export class OrdersService {
 
   /**
    * Calls the API to update the status of an order.
-   * 
+   *
    * @param order - the order that needs to be updated.
    * @returns an observable with the updated order.
    */
