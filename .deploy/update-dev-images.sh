d624998c7dac3131356bd0accc86bff465d436ce
IroniX2 <57399961+IroniX2@users.noreply.github.com>
2021-12-08 10:02:42 +0100

    docs: TC-35 Add comments to the deployment scripts

--- .deploy/update-dev-images.sh
+++ .deploy/update-dev-images.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # run 'chmod u+x update-prod-images.sh' to make it executable
+# This must be run from project root as the paths are based on that
+# The dockerhub token must be present in .deploy/.env-files/.env.dockerhub
+#  or the execution will fail as the credentials are required to push to 
+#  a private repository on dockerhub
+
+# Example usage: .deploy/update-dev-images.sh
 
 # Logs in to docker hub
 cat .deploy/.env-files/.env.dockerhub | docker login --username kwandes --password-stdin;
