db8baa16c7ac8cc46482c46c969d8d211a6e7bf3
IroniX2 <57399961+IroniX2@users.noreply.github.com>
2021-12-08 10:19:36 +0100

    docs: TC-35 Remove redundant comments and add further explanations

--- .deploy/update-dev-images.sh
+++ .deploy/update-dev-images.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # run 'chmod u+x update-prod-images.sh' to make it executable
+
+# This file is intended to be run locally and not on the deployment environment
+# It can be used to push images with local changes to DockerHub faster since
+#  the build server is quite slow compared to our machines
+
 # This must be run from project root as the paths are based on that
 # The dockerhub token must be present in .deploy/.env-files/.env.dockerhub
 #  or the execution will fail as the credentials are required to push to 
