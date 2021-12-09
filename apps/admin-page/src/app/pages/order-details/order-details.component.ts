a011286b0ad02b59d4e49597dd9410cd6c197a65
Cristian <cristi.valentin29@gmail.com>
2021-12-09 07:44:33 +0100

    refactor: TC-28 extract logic in order service

--- apps/admin-page/src/app/pages/order-details/order-details.component.ts
+++ apps/admin-page/src/app/pages/order-details/order-details.component.ts
@@ -47,7 +47,7 @@ export class OrderDetailsComponent implements OnInit {
     OrderStatusEnum.rejected,
   ];
 
-  constructor(private ordersService: OrdersService, private route: ActivatedRoute, private location: Location) {
+  constructor(public ordersService: OrdersService, private route: ActivatedRoute, private location: Location) {
     this.title = 'Loading...';
   }
 
@@ -125,51 +125,10 @@ export class OrderDetailsComponent implements OnInit {
           this.items = this.order?.transactionItems;
         }
         if (this.order) {
-          this.daysLeft = this.getDaysLeft(this.order?.createdAt);
+          this.daysLeft = this.ordersService.getDaysLeft(this.order?.createdAt);
         }
       },
     });
-  }
-
-  /**
-   * Calculates how many days there are left until the delivery date.
-   *
-   * @param orderDate The date when the order has been placed.
-   * @returns amount of days left until delivery date.
-   */
-  getDaysLeft(orderDate: Date): number {
-    // Calculate delivery date. (can take at most 14 days after the order was placed)
-    const deliveryDate = new Date(orderDate);
-    deliveryDate.setDate(deliveryDate.getDate() + 14);
-
-    // Calculate how many days are left
-    return Number(((deliveryDate.getTime() - Date.now()) / (1000 * 3600 * 24)).toFixed(0));
-  }
-
-  /**
-   * Gets the Label color for the 'Days Left' column in the table based on the days left until
-   * delivery and the status of the order.
-   *
-   * @param daysLeft days left until delivery.
-   * @param orderStatus the status of the order.
-   * @returns the color of the label.
-   */
-  getDaysLeftColor(daysLeft: number, orderStatus: OrderStatusEnum): LabelColorsEnum {
-    // Order status is NOT: Delivered, Shipped or Rejected.
-    if (
-      orderStatus !== OrderStatusEnum.delivered &&
-      orderStatus !== OrderStatusEnum.shipped &&
-      orderStatus !== OrderStatusEnum.rejected
-    ) {
-      if (daysLeft >= 9) {
-        return LabelColorsEnum.green;
-      }
-      if (daysLeft >= 5) {
-        return LabelColorsEnum.yellow;
-      }
-      return LabelColorsEnum.red;
-    }
-    return LabelColorsEnum.lightGrey;
   }
 
   /**
@@ -179,7 +138,7 @@ export class OrderDetailsComponent implements OnInit {
    * @param dimension - the dimension of the transaction item.
    * @returns the price of the item.
    */
-  calculateItemPrice(quantity: number, dimension: DesignDimensionEnum): number {
+  getItemPrice(quantity: number, dimension: DesignDimensionEnum): number {
     switch (dimension) {
       case DesignDimensionEnum.small:
         return quantity * 495;
@@ -227,43 +186,4 @@ export class OrderDetailsComponent implements OnInit {
     }
     return 0;
   }
-
-  // TODO: extract the status color related functions to a helper/util function.
-  /**
-   * Gets the Label color of the Order Status based on the status.
-   *
-   * @param orderStatus the status of the order.
-   * @returns the color of the label.
-   */
-  getOrderStatusColor(orderStatus: OrderStatusEnum): LabelColorsEnum {
-    switch (orderStatus) {
-      case OrderStatusEnum.initial:
-        return LabelColorsEnum.blue;
-      case OrderStatusEnum.pending:
-        return LabelColorsEnum.blue;
-      case OrderStatusEnum.new:
-        return LabelColorsEnum.red;
-      case OrderStatusEnum.rejected:
-        return LabelColorsEnum.grey;
-      case OrderStatusEnum.processed:
-        return LabelColorsEnum.red;
-      case OrderStatusEnum.assembling:
-        return LabelColorsEnum.yellow;
-      case OrderStatusEnum.shipped:
-        return LabelColorsEnum.blue;
-      case OrderStatusEnum.delivered:
-        return LabelColorsEnum.green;
-      default:
-        return LabelColorsEnum.lightGrey;
-    }
-  }
-}
-
-enum LabelColorsEnum {
-  red = '#FF0000',
-  green = '#00B207',
-  blue = '#6D7CFF',
-  yellow = '#F4DC00',
-  grey = '#ABABAB',
-  lightGrey = '#F1F1F1',
 }
