1bc6f2a21004547b1c190b377cdc4ce701bce5bc
Cristian <cristi.valentin29@gmail.com>
2021-12-13 09:08:52 +0100

    feat: TC-25 add discounts table

--- apps/admin-page/src/app/pages/discounts/discounts.component.ts
+++ apps/admin-page/src/app/pages/discounts/discounts.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IDiscount } from '@interfaces';
+import { DiscountsService } from '../../services/discounts/discounts.service';
 
 @Component({
   selector: 'webstore-discounts',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./discounts.component.css']
 })
 export class DiscountsComponent implements OnInit {
+  isLoading = true;
+  displayedColumns: string[] = [
+    'code',
+    'id',
+    'usesLeft',
+    'used',
+    'expiresAt',
+    'actions',
+  ];
+  discounts!: IDiscount[];
 
-  constructor() { }
+  constructor(private discountsService: DiscountsService) { }
 
+  /**
+   * Fetches the discounts.
+   */
   ngOnInit(): void {
+    this.fetchDiscounts();
+  }
+
+  /**
+   * Fetches the discounts from the API.\
+   * \
+   * Will toggle `isLoading` to `true` while the discounts are being fetched
+   * and revert it to `false` when they have been fetched.\
+   * \
+   * **Uses**: `discountsService` -> to call the API.
+   */
+   fetchDiscounts(): void {
+    this.isLoading = true;
+    this.discountsService.getDiscounts().subscribe({
+      error: (error: HttpErrorResponse) => {
+        console.error(error);
+      },
+      next: (discounts: IDiscount[]) => {
+        this.isLoading = false;
+        this.discounts = discounts;
+      },
+    });
   }
 
 }
