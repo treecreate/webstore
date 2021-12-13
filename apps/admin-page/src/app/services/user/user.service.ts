44fe7f10e45cc0c5d1588332bf5e279756331124
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-12-13 10:03:02 +0100

    feat: 🎸 Add JDC for user service again

--- apps/admin-page/src/app/services/user/user.service.ts
+++ apps/admin-page/src/app/services/user/user.service.ts
@@ -11,20 +11,32 @@ import { environment as env } from '../../../environments/environment';
 export class UserService {
   constructor(private http: HttpClient, private localStorageService: LocalStorageService) {}
 
+  /**
+   * Call the api to get the currently authentikated user.
+   *
+   * @returns the user as an observable.
+   */
   public getCurrentUser(): Observable<IUser> {
     return this.http.get<IUser>(`${env.apiUrl}/users/me`);
   }
 
+  /**
+   * Call api to update a user. Send all the user info needed. None of the fields are *required* for the call.
+   *
+   * @param params ( UpdateUserRequest )
+   * @returns the updated user as an observable.
+   */
   public updateUser(params: UpdateUserRequest): Observable<IUser> {
     return this.http.put<IUser>(`${env.apiUrl}/users`, params);
   }
 
   /**
+   * Calls api to update a users password.
    *
-   * @param params ()
-   * @returns
+   * @param params ( UpdatePasswordAsAdminRequest = { userId: string, password: string } )
+   * @returns the updated user as an observable.
    */
-  public updatePassword(params: UpdatePasswordAsAdminRequest) {
+  public updatePassword(params: UpdatePasswordAsAdminRequest): Observable<IUser> {
     return this.http.put<IUser>(`${env.apiUrl}/users/${params.userId}`, { password: params.password });
   }
 }
