45ea46d190718ccb52e9b6760cf00e9e4a846c49
Cristian <cristi.valentin29@gmail.com>
2021-12-13 11:40:49 +0100

    test: TC-25 add tests for discounts page

--- apps/admin-page/src/app/pages/discounts/discounts.component.spec.ts
+++ apps/admin-page/src/app/pages/discounts/discounts.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { DiscountsComponent } from './discounts.component';
 
@@ -8,6 +9,7 @@ describe('DiscountsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ DiscountsComponent ]
     })
     .compileComponents();
