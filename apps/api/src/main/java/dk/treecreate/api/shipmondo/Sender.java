aeded95cda94c30813a147cbab380c06ed43b573
IroniX2 <57399961+IroniX2@users.noreply.github.com>
2021-12-13 12:32:29 +0100

    refactor: TC-51 Move Address to the newly created utility folder

--- apps/api/src/main/java/dk/treecreate/api/shipmondo/Sender.java
+++ apps/api/src/main/java/dk/treecreate/api/shipmondo/Sender.java
@@ -1,5 +1,7 @@
 package dk.treecreate.api.shipmondo;
 
+import dk.treecreate.api.shipmondo.utility.Address;
+
 public class Sender {
    
     private Address address;    
--- apps/api/src/main/java/dk/treecreate/api/shipmondo/utility/Address.java
+++ apps/api/src/main/java/dk/treecreate/api/shipmondo/utility/Address.java
@@ -1,4 +1,4 @@
-package dk.treecreate.api.shipmondo;
+package dk.treecreate.api.shipmondo.utility;
 
 public class Address {
 
