08cca2414c317452fd4c34109c71b13db7115bfe
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-12-16 10:43:37 +0100

    feat: 🎸 Add check to updateUser in account info page

--- apps/admin-page/src/app/pages/account/account.component.ts
+++ apps/admin-page/src/app/pages/account/account.component.ts
@@ -2,12 +2,13 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { IUser } from '@interfaces';
+import { IAuthUser, IUser } from '@interfaces';
 import { UserRoles } from '@models';
 import { UserService } from '../../services/user/user.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ChangePasswordDialogComponent } from '../../components/change-password-dialog/change-password-dialog.component';
 import { ActivatedRoute, Route, Router } from '@angular/router';
+import { AuthService } from '../../services/authentication/auth.service';
 
 @Component({
   selector: 'webstore-account',
@@ -16,6 +17,7 @@ import { ActivatedRoute, Route, Router } from '@angular/router';
 })
 export class AccountComponent {
   public user?: IUser;
+  public authUser;
   public accountForm: FormGroup;
   public isLoading = false;
   public isUpdatingInfo = false;
@@ -31,14 +33,18 @@ export class AccountComponent {
    */
   constructor(
     private userService: UserService,
+    private authService: AuthService,
     private snackBar: MatSnackBar,
     public dialog: MatDialog,
     private route: ActivatedRoute,
     private router: Router
   ) {
     this.isLoading = true;
-
+    // get authUser
+    this.authUser = this.authService.getAuthUser();
+    // get user info
     const queryParams = this.route.snapshot.queryParams;
+    // check if it is findUserById
     if (queryParams.userId != undefined) {
       this.userService.getUser(queryParams.userId).subscribe(
         (user: IUser) => {
@@ -53,6 +59,7 @@ export class AccountComponent {
           this.isLoading = false;
         }
       );
+      // Get authentikated users information
     } else {
       this.userService.getCurrentUser().subscribe(
         (user: IUser) => {
@@ -148,29 +155,66 @@ export class AccountComponent {
    */
   updateAccount(): void {
     this.isUpdatingInfo = true;
-    this.userService
-      .updateUser({
-        name: this.accountForm.get('name')?.value,
-        phoneNumber: this.accountForm.get('phoneNumber')?.value,
-        email: this.accountForm.get('email')?.value,
-        streetAddress: this.accountForm.get('streetAddress')?.value,
-        streetAddress2: this.accountForm.get('streetAddress2')?.value,
-        city: this.accountForm.get('city')?.value,
-        postcode: this.accountForm.get('postcode')?.value,
-      })
-      .subscribe(
-        (data: IUser) => {
-          console.log(data);
-          this.snackBar.open('Your account has been updated!', `I'm the best`, { duration: 5000 });
-          this.user = data;
-          this.isUpdatingInfo = false;
-        },
-        (err: HttpErrorResponse) => {
-          console.log(err.message);
-          this.snackBar.open('Updating the user data failed', 'Oh no!', { duration: 5000 });
-          this.isUpdatingInfo = false;
-        }
-      );
+    // Check if authenticated user is same as user being edited
+    if (this.user?.email === this.authUser?.email) {
+      // Update current users account
+      this.userService
+        .updateUser({
+          name: this.accountForm.get('name')?.value,
+          phoneNumber: this.accountForm.get('phoneNumber')?.value,
+          email: this.accountForm.get('email')?.value,
+          streetAddress: this.accountForm.get('streetAddress')?.value,
+          streetAddress2: this.accountForm.get('streetAddress2')?.value,
+          city: this.accountForm.get('city')?.value,
+          postcode: this.accountForm.get('postcode')?.value,
+        })
+        .subscribe(
+          (data: IUser) => {
+            console.log(data);
+            this.snackBar.open('Your account has been updated!', `I'm the best`, { duration: 5000 });
+            this.user = data;
+            this.isUpdatingInfo = false;
+          },
+          (err: HttpErrorResponse) => {
+            console.log(err.message);
+            this.snackBar.open('Updating the user data failed', 'Oh no!', { duration: 5000 });
+            this.isUpdatingInfo = false;
+          }
+        );
+      if (this.accountForm.get('email')?.value != this.authUser?.email) {
+        this.snackBar.open('You have been logged out due to email change', 'Wauw');
+        this.authService.logout();
+        this.router.navigate(['/login']);
+      }
+    } else {
+      // update a users account by id
+      this.userService
+        .updateUserById(
+          {
+            name: this.accountForm.get('name')?.value,
+            phoneNumber: this.accountForm.get('phoneNumber')?.value,
+            email: this.accountForm.get('email')?.value,
+            streetAddress: this.accountForm.get('streetAddress')?.value,
+            streetAddress2: this.accountForm.get('streetAddress2')?.value,
+            city: this.accountForm.get('city')?.value,
+            postcode: this.accountForm.get('postcode')?.value,
+          },
+          this.user!.userId
+        )
+        .subscribe(
+          (data: IUser) => {
+            console.log(data);
+            this.snackBar.open('Your account has been updated!', `I'm the best`, { duration: 5000 });
+            this.user = data;
+            this.isUpdatingInfo = false;
+          },
+          (err: HttpErrorResponse) => {
+            console.log(err.message);
+            this.snackBar.open('Updating the user data failed', 'Oh no!', { duration: 5000 });
+            this.isUpdatingInfo = false;
+          }
+        );
+    }
   }
 
   /**
