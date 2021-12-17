3f82700ef8c1991f880cce081e9d61579ff15185
Cristian <cristi.valentin29@gmail.com>
2021-12-17 13:34:54 +0100

    feat: TC-48 add edit discount page with tests

--- apps/admin-page/src/app/pages/edit-discount/edit-discount.component.spec.ts
+++ apps/admin-page/src/app/pages/edit-discount/edit-discount.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { EditDiscountComponent } from './edit-discount.component';
 
@@ -8,6 +10,7 @@ describe('EditDiscountComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule],
       declarations: [ EditDiscountComponent ]
     })
     .compileComponents();
--- apps/admin-page/src/app/pages/edit-discount/edit-discount.component.ts
+++ apps/admin-page/src/app/pages/edit-discount/edit-discount.component.ts
@@ -1,15 +1,77 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { IDiscount } from '@interfaces';
+import { Location } from '@angular/common';
+import { DiscountsService } from '../../services/discounts/discounts.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'webstore-edit-discount',
   templateUrl: './edit-discount.component.html',
-  styleUrls: ['./edit-discount.component.css']
+  styleUrls: ['./edit-discount.component.css'],
 })
 export class EditDiscountComponent implements OnInit {
+  discount?: IDiscount;
+  id?: string;
+  isLoading = true;
 
-  constructor() { }
+  // Discount information
+  discountCodeControl!: FormControl;
+  usesLeftControl!: FormControl;
+  expirationDateControl!: FormControl;
+  amountControl!: FormControl;
+  typeControl!: FormControl;
+  startDateControl!: FormControl;
+
+  constructor(public discountService: DiscountsService, private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id') || undefined;
+    // Fetching the discount.
+    if (this.id !== undefined) {
+      this.fetchDiscount(this.id);
+    }
   }
 
+  /**
+   * Redirects to the previous page.
+   */
+  historyBack(): void {
+    this.location.back();
+  }
+
+  /**
+   * Fetches a discount from the API.\
+   * \
+   * Will toggle `isLoading` to `true` while the discount is being fetched
+   * and revert it to `false` when it has been fetched.\
+   * Initializes all of the discount related values.\
+   * \
+   * **Uses**: `discountService` -> to call the API.
+   */
+  fetchDiscount(id: string): void {
+    this.isLoading = true;
+    this.discountService.getDiscounts().subscribe({
+      error: (error: HttpErrorResponse) => {
+        console.error(error);
+      },
+      next: (discounts: IDiscount[]) => {
+        this.isLoading = false;
+        this.discount = discounts.find((discount) => discount.discountId === id);
+
+        // Discount Information
+        this.discountCodeControl = new FormControl(this.discount?.discountCode);
+        this.usesLeftControl = new FormControl(this.discount?.remainingUses);
+        if (this.discount?.expiresAt) {
+          this.expirationDateControl = new FormControl(new Date(this.discount?.expiresAt).toLocaleDateString());
+        }
+        this.amountControl = new FormControl(this.discount?.amount);
+        this.typeControl = new FormControl(this.discount?.type);
+        if (this.discount?.createdAt) {
+          this.startDateControl = new FormControl(new Date(this.discount.createdAt).toLocaleDateString());
+        }
+      },
+    });
+  }
 }
