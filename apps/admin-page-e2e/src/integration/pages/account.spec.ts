d9a4d63541563345eaa2475b037a9ab986858cb2
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-12-19 09:25:09 +0100

    test: 💍 Fix e2e test in account

--- apps/admin-page-e2e/src/integration/pages/account.spec.ts
+++ apps/admin-page-e2e/src/integration/pages/account.spec.ts
@@ -1,11 +1,24 @@
-import { IUser } from '@interfaces';
+import { IRole, IUser } from '@interfaces';
 import { LocalStorageVars, UserRoles } from '@models';
 import { AuthenticationService, AuthUserEnum } from '@webstore/mocks';
+
+const roleAdmin: IRole = {
+  roleId: '1234',
+  name: UserRoles.admin,
+};
+const roleDeveloper: IRole = {
+  roleId: '1234',
+  name: UserRoles.developer,
+};
+const roleUser: IRole = {
+  roleId: '1234',
+  name: UserRoles.user,
+};
 
 const mockAdminUser: IUser = {
   userId: 'PleaseTakeMe',
   email: 'e2e@test.com',
-  roles: [UserRoles.user, UserRoles.admin],
+  roles: [roleAdmin, roleDeveloper, roleUser],
   name: 'telinior the great',
   phoneNumber: '69420420',
   streetAddress: 'Y u b like dis?',
