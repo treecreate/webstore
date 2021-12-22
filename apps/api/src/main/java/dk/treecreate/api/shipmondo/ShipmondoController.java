db70c7354fa61e7278e8d10eaf9ee2e62d280511
IroniX2 <57399961+IroniX2@users.noreply.github.com>
2021-12-22 13:44:39 +0100

    feat: TC-51 Tweak shipmondo controller with new functions

--- apps/api/src/main/java/dk/treecreate/api/shipmondo/ShipmondoController.java
+++ apps/api/src/main/java/dk/treecreate/api/shipmondo/ShipmondoController.java
@@ -1,7 +1,9 @@
 package dk.treecreate.api.shipmondo;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -12,7 +14,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.UnknownContentTypeException;
+import org.springframework.web.server.ResponseStatusException;
 
 import dk.treecreate.api.shipmondo.dto.ShipmentObjectDto;
 import dk.treecreate.api.shipmondo.shipment_object_components.Parcels;
@@ -29,31 +34,36 @@ public class ShipmondoController
     {
         var receiverAddress = new Address("Kongens Nytorv 14", "1050", "Copenhagen", "DK");
         var receiverContact = new ContactInfo("Test contact", null, null, "52525252", "test@test.email");
+        var parcelsList = new ArrayList<>(Arrays.asList(new Parcels(1, 1000)));
 
-        var parcels = new Parcels(1, 1000);
-        var parcelsList = new ArrayList<Parcels>();
-        parcelsList.add(parcels);
+        var shipment = ShipmentObjectDto.createShipment("", receiverContact, receiverAddress, parcelsList);
+        var response = queryShipmondo(shipment);
 
-        var object = ShipmentObjectDto.createShipment("", receiverContact, receiverAddress, parcelsList);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
-        try
-        {
+    private ShipmentObjectResponse queryShipmondo(ShipmentObject shipment) {
+
             HttpHeaders headers = new HttpHeaders();
+            RestTemplate restTemplate = new RestTemplate();
+
             headers.setContentType(MediaType.APPLICATION_JSON);
             headers.set("Authorization",
                     "");
+        try {
             URI uri = new URI("https://app.shipmondo.com/api/public/v3/shipments");
 
-            HttpEntity<ShipmentObject> httpEntity = new HttpEntity<>(object, headers);
-
-            RestTemplate restTemplate = new RestTemplate();
-            var response = restTemplate.postForObject(uri, httpEntity, ShipmentObjectResponse.class);
-            System.out.println(response);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } catch (Exception e)
-        {
-            System.err.println("Its a no go buddy!\n" + e);
-            throw e;
+            HttpEntity<ShipmentObject> httpEntity = new HttpEntity<>(shipment, headers);
+            return restTemplate.postForObject(uri, httpEntity, ShipmentObjectResponse.class);
+            
+        } catch (URISyntaxException | ResourceAccessException e) {
+            System.err.println(e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Invalid Shipmondo URI.");
+        } catch (UnknownContentTypeException e) {
+            System.err.println(e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to parse response");
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid request.\n" + e);
         }
     }
 }
