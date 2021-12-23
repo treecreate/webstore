6816aedc4350a3b2eb8c0d3bbe280573c820bbd7
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-12-23 16:55:30 +0100

    fix: 🐛 failing ci tests

--- apps/admin-page/src/app/components/view-user-orders-dialog/view-user-orders-dialog.component.spec.ts
+++ apps/admin-page/src/app/components/view-user-orders-dialog/view-user-orders-dialog.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ViewUserOrdersDialogComponent } from './view-user-orders-dialog.component';
@@ -8,7 +9,7 @@ describe('ViewUserOrdersDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ViewUserOrdersDialogComponent],
+      declarations: [ViewUserOrdersDialogComponent, HttpClientModule],
     }).compileComponents();
   });
 
