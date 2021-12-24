9da65ae90b6a7e1936e01149f69141cff169a6c0
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-12-24 14:02:33 +0100

    test: 💍 Add e2e tests to users page

--- apps/admin-page-e2e/src/integration/pages/users.spec.ts
+++ apps/admin-page-e2e/src/integration/pages/users.spec.ts
@@ -1,3 +1,4 @@
+import { CurrencyEnum, DiscountType, IOrder, OrderStatusEnum, ShippingMethodEnum } from '@interfaces';
 import { LocalStorageVars, UserRoles } from '@models';
 import { AuthenticationService, AuthUserEnum } from '@webstore/mocks';
 
@@ -54,6 +55,89 @@ const mockUsers = [
   },
 ];
 
+const mockOrders: IOrder[] = [
+  {
+    status: OrderStatusEnum.assembling,
+    billingInfo: {
+      city: 'cph',
+      country: 'Denmark',
+      email: 'example@hotdeals.dev',
+      name: 'John Doe',
+      phoneNumber: '+4512345678',
+      postcode: '9999',
+      streetAddress: 'StreetGade 123',
+    },
+    contactInfo: {
+      city: 'cph',
+      country: 'Denmark',
+      email: 'example@hotdeals.dev',
+      name: 'John Doe',
+      phoneNumber: '+4512345678',
+      postcode: '9999',
+      streetAddress: 'StreetGade 123',
+    },
+    createdAt: new Date(),
+    currency: CurrencyEnum.dkk,
+    discount: {
+      discountCode: 'suck it',
+      type: DiscountType.amount,
+      amount: 100,
+      remainingUses: 1,
+      totalUses: 2,
+      isEnabled: true,
+    },
+    orderId: 'MakeMeWantIt',
+    paymentId: 'c0a80121-7ac0-190b-812a1-c08ab0a12345',
+    plantedTrees: 1,
+    shippingMethod: ShippingMethodEnum.homeDelivery,
+    //State    -- not implemented
+    subtotal: 1914,
+    total: 1814,
+    transactionItems: [],
+    userId: 'IllTakeYouSon',
+  },
+  {
+    status: OrderStatusEnum.assembling,
+    billingInfo: {
+      city: 'cph',
+      country: 'Denmark',
+      email: 'example@hotdeals.dev',
+      name: 'John Doe',
+      phoneNumber: '+4512345678',
+      postcode: '9999',
+      streetAddress: 'StreetGade 123',
+    },
+    contactInfo: {
+      city: 'cph',
+      country: 'Denmark',
+      email: 'example@hotdeals.dev',
+      name: 'John Doe',
+      phoneNumber: '+4512345678',
+      postcode: '9999',
+      streetAddress: 'StreetGade 123',
+    },
+    createdAt: new Date(),
+    currency: CurrencyEnum.dkk,
+    discount: {
+      discountCode: 'suck it',
+      type: DiscountType.amount,
+      amount: 100,
+      remainingUses: 1,
+      totalUses: 2,
+      isEnabled: true,
+    },
+    orderId: 'MakeMeWantIt',
+    paymentId: 'c0a80121-7ac0-190b-812a1-c08ab0a12345',
+    plantedTrees: 1,
+    shippingMethod: ShippingMethodEnum.homeDelivery,
+    //State    -- not implemented
+    subtotal: 1914,
+    total: 1814,
+    transactionItems: [],
+    userId: 'IllTakeYouSon',
+  },
+];
+
 describe('Users page', () => {
   beforeEach(() => {
     localStorage.setItem(LocalStorageVars.authUser, JSON.stringify(authMockService.getMockUser(AuthUserEnum.authUser)));
@@ -66,6 +150,39 @@ describe('Users page', () => {
     cy.visit('/users');
   });
 
-  it('it should display the users in the list', () => {});
-  it('it should display view orders email', () => {});
+  it('it should display the users in the list', () => {
+    cy.get('[data-cy=users-table]').should('exist');
+    cy.get('[data-cy=user-email]').should('have.length', 4);
+  });
+  it('it should display orders list dialog when clicking view orders', () => {
+    cy.intercept('GET', '/orders', {
+      body: mockOrders,
+      statusCode: 200,
+    });
+    cy.get('[data-cy=view-orders-dialog]').should('not.exist');
+    cy.get('[data-cy=view-user-orders-btn]').first().click();
+    cy.get('[data-cy=view-orders-dialog]').should('exist');
+    cy.get('[data-cy=user-has-no-orders]').should('not.exist');
+    cy.get('[data-cy=user-orders-table]').should('exist');
+  });
+
+  it('it should display orders list dialog with no orders', () => {
+    cy.intercept('GET', '/orders', {
+      body: [],
+      statusCode: 200,
+    });
+    cy.get('[data-cy=view-orders-dialog]').should('not.exist');
+    cy.get('[data-cy=view-user-orders-btn]').first().click();
+    cy.get('[data-cy=view-orders-dialog]').should('exist');
+    cy.get('[data-cy=user-has-no-orders]').should('exist');
+  });
+
+  it('it should be able to view details about the user when clicking the view button', () => {
+    cy.intercept('GET', '/users/' + mockUsers[0].userId, {
+      body: mockUsers[0],
+      statusCode: 200,
+    });
+    cy.get('[data-cy=edit-user-btn]').first().click();
+    cy.url().should('contain', '/account?userId=IllTakeYouSon');
+  });
 });
