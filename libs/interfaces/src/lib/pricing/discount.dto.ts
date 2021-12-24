353bb90b45b6d34af3b45f0ca906e4a4bb3d68ac
Kwandes <bz.jasiek@gmail.com>
2021-12-24 09:26:35 +0100

    feat: TC-61 add updateDiscount method to discount service

---
+++ libs/interfaces/src/lib/pricing/discount.dto.ts
@@ -0,0 +1,12 @@
+import { DiscountType } from ".";
+
+export interface UpdateDiscountRequest {
+    discountCode?: string;
+    type?: DiscountType;
+    amount?: number;
+    remainingUses?: number;
+    totalUses?: number;
+    isEnabled?: boolean;
+    startsAt?: Date;
+    expiresAt?: Date;
+  }
