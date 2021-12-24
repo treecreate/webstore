2ea7c6a5a0ef8c36535bd9593b9cef579e39e4a9
Kwandes <bz.jasiek@gmail.com>
2021-12-24 09:46:29 +0100

    feat: TC-61 add logic for updating the discount

--- apps/admin-page/src/app/pages/edit-discount/edit-discount.component.ts
+++ apps/admin-page/src/app/pages/edit-discount/edit-discount.component.ts
@@ -2,6 +2,7 @@ import { Location } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { DiscountType, IDiscount } from '@interfaces';
 import { DiscountsService } from '../../services/discounts/discounts.service';
@@ -25,8 +26,14 @@ export class EditDiscountComponent implements OnInit {
    * @param discountService service for discount-related http calls and logic
    * @param route angular route for getting url params
    * @param location used for navigating through browser history
+   * @param snackBar material UI module for alerts
    */
-  constructor(public discountService: DiscountsService, private route: ActivatedRoute, private location: Location) {
+  constructor(
+    public discountService: DiscountsService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private snackBar: MatSnackBar
+  ) {
     this.discountForm = new FormGroup({
       discountCode: new FormControl('', [Validators.required, Validators.maxLength(50)]),
       usesLeft: new FormControl('', [Validators.required, Validators.min(0)]),
@@ -71,15 +78,61 @@ export class EditDiscountComponent implements OnInit {
         this.isLoading = false;
         this.discount = discount;
 
-        this.discountForm.patchValue({
-          discountCode: discount.discountCode,
-          usesLeft: discount.remainingUses,
-          expiresAt: this.discount?.expiresAt || '',
-          startsAt: this.discount?.startsAt || '',
-          amount: discount.amount,
-          type: discount.type,
-        });
+        this.patchDiscountForm(discount);
       },
+    });
+  }
+
+  /**
+   * Performs a API call in order to update the discount entity based on the discount form information.\
+   * Changes the state of isLoading variable whilst the update is on-going
+   */
+  updateDiscount(): void {
+    if (!this.isFormValid && this.isFormDirty) {
+      return;
+    }
+    this.isLoading = true;
+    this.discountService
+      .updateDiscount(this.discount?.discountId!, {
+        discountCode: this.discountForm.get('discountCode')?.value,
+        remainingUses: this.discountForm.get('usesLeft')?.value,
+        expiresAt: this.discountForm.get('expiresAt')?.value,
+        startsAt: this.discountForm.get('startsAt')?.value,
+        amount: this.discountForm.get('amount')?.value,
+        type: this.discountForm.get('type')?.value,
+      })
+      .subscribe({
+        error: (error: HttpErrorResponse) => {
+          console.error(error);
+          this.snackBar.open(
+            `Failed to update the discount with error: ${error.error.error}`,
+            `Cool, Let's try again`,
+            { duration: 5000 }
+          );
+          this.isLoading = false;
+        },
+        next: (discount: IDiscount) => {
+          this.discountForm.markAsPristine();
+          this.snackBar.open(`Discount has been updated`);
+          this.patchDiscountForm(discount);
+          this.isLoading = false;
+        },
+      });
+    console.log('Discount form: ', this.discountForm?.get('discountCode')?.value);
+  }
+
+  /**
+   * Updates the discount form with the provided data
+   * @param discount discount information
+   */
+  patchDiscountForm(discount: IDiscount): void {
+    this.discountForm.patchValue({
+      discountCode: discount.discountCode,
+      usesLeft: discount.remainingUses,
+      expiresAt: this.discount?.expiresAt || '',
+      startsAt: this.discount?.startsAt || '',
+      amount: discount.amount,
+      type: discount.type,
     });
   }
 
