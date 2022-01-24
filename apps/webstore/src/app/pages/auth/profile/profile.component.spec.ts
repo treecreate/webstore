c5c334eeb0825d1cf58e3d2dc27b8b5d61aebb40
Kwandes <bz.jasiek@gmail.com>
2022-01-24 20:56:24 +0100

    refactor: 💡 Adjust usages of roles in test files

--- apps/webstore/src/app/pages/auth/profile/profile.component.spec.ts
+++ apps/webstore/src/app/pages/auth/profile/profile.component.spec.ts
@@ -3,7 +3,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { IUser } from '@interfaces';
 import { UserRoles } from '@models';
-
 import { ProfileComponent } from './profile.component';
 
 describe('ProfileComponent', () => {
@@ -12,7 +11,7 @@ describe('ProfileComponent', () => {
   const mockUser: IUser = {
     userId: '1',
     email: 'e2e@test.com',
-    roles: [UserRoles.user],
+    roles: [{ name: UserRoles.user, roleId: '' }],
     name: 'teodor jonasson',
     phoneNumber: '',
     streetAddress: '',
