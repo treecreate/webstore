ede179aca4e2446dbb08a4686993786db0674740
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-03-11 20:29:22 +0100

    test: 💍 Fix ci tests for template modal

--- apps/webstore/src/app/shared/components/modals/family-tree-template-modal/family-tree-template-modal.component.spec.ts
+++ apps/webstore/src/app/shared/components/modals/family-tree-template-modal/family-tree-template-modal.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { FamilyTreeTemplateModalComponent } from './family-tree-template-modal.component';
 
@@ -9,6 +11,8 @@ describe('FamilyTreeTemplateModalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [FamilyTreeTemplateModalComponent],
+      imports: [NgbModule, RouterTestingModule],
+      providers: [NgbActiveModal],
     }).compileComponents();
   });
 
