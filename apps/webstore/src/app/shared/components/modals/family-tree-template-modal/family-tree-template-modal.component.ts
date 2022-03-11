7b3833f33fb49723c40e1105032b09021445cdd7
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-03-11 20:16:09 +0100

    style: 💄 Apply formatting

--- apps/webstore/src/app/shared/components/modals/family-tree-template-modal/family-tree-template-modal.component.ts
+++ apps/webstore/src/app/shared/components/modals/family-tree-template-modal/family-tree-template-modal.component.ts
@@ -30,7 +30,9 @@ export class FamilyTreeTemplateModalComponent {
    * @param name
    */
   applyTemplate(name: string) {
-    const selectedTemplate: ITemplateFamilyTree = templates.concat(templateExtra).find((template) => template.name === name);
+    const selectedTemplate: ITemplateFamilyTree = templates
+      .concat(templateExtra)
+      .find((template) => template.name === name);
     console.log(selectedTemplate.designProperties);
     this.localStorageService.setItem<IFamilyTree>(LocalStorageVars.designFamilyTree, selectedTemplate.designProperties);
     this.router.navigate(['/product']);
