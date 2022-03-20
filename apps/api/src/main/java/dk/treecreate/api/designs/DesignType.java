e77d23c557272bd705923fe0df693df204a28d28
Kwandes <bz.jasiek@gmail.com>
2022-03-20 02:31:34 +0100

    feat: 🎸 Add support of quotable as design type in the API
    Pricing calculations are not yet updated

--- apps/api/src/main/java/dk/treecreate/api/designs/DesignType.java
+++ apps/api/src/main/java/dk/treecreate/api/designs/DesignType.java
@@ -4,7 +4,8 @@ public enum DesignType
 {
     // if the order of these ever gets changed, the saved designs have to be updated since the entry uses the enum index (0 = family tree etc)
     // If you don't update the saved designs, they will be returning a wrong (old) design type index
-    FAMILY_TREE("FAMILY_TREE");
+    FAMILY_TREE("FAMILY_TREE"),
+    QUOTABLE("QUOTABLE");
 
     public final String label;
 
