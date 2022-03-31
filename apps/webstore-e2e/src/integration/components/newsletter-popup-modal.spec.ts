9ed4528b16ba6919cd9563be447e2350bdbe5c81
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-03-31 19:37:10 +0200

    test: 💍 Update e2e tests

--- apps/webstore-e2e/src/integration/components/newsletter-popup-modal.spec.ts
+++ apps/webstore-e2e/src/integration/components/newsletter-popup-modal.spec.ts
@@ -8,17 +8,15 @@ describe('Signup to newsletter popup modal', () => {
   it('should show the popup and save it in localstorage', () => {
     expect(localStorage.getItem(LocalStorageVars.hasSeenNewsletterModal)).to.equal(null);
     cy.get('[data-cy=cookie-prompt-modal-accept-cookies-btn]').click();
-    /*eslint-disable */
-    setTimeout(() => {
-      cy.get('[data-cy=newsletter-modal-popup]').should('not.exist');
-      cy.get('[data-cy=newsletter-modal-popup]').should('exist');
-      cy.get('[data-cy=newsletter-modal-close-btn]')
-        .click()
-        .then(() => {
-          expect(localStorage.getItem(LocalStorageVars.hasSeenNewsletterModal)).to.equal('true');
-        });
-    }, 4000);
-    /*eslint-enable */
+    // eslint-disable-next-line
+    cy.wait(4000);
+    cy.get('[data-cy=newsletter-modal-popup]').should('not.exist');
+    cy.get('[data-cy=newsletter-modal-popup]').should('exist');
+    cy.get('[data-cy=newsletter-modal-close-btn]')
+      .click()
+      .then(() => {
+        expect(localStorage.getItem(LocalStorageVars.hasSeenNewsletterModal)).to.equal('true');
+      });
   });
 
   it('registers a new newsletter signup', () => {
@@ -27,15 +25,13 @@ describe('Signup to newsletter popup modal', () => {
     });
     cy.get('[data-cy=cookie-prompt-modal-accept-cookies-btn]').click();
     cy.get('[data-cy=newsletter-modal-popup]').should('not.exist');
-    /*eslint-disable */
-    setTimeout(() => {
-      cy.get('[data-cy=newsletter-modal-popup]').should('exist');
-      cy.get('[data-cy=newsletter-modal-email-input]').type('test');
-      cy.get('[data-cy=newsletter-modal-subscribe-btn]').click();
-      cy.get('[data-cy=newsletter-modal-email-input]').type('@test.com');
-      cy.get('[data-cy=newsletter-modal-subscribe-btn]').click();
-      cy.get('[data-cy=newsletter-modal-popup]').should('not.exist');
-    }, 4000);
-    /*eslint-enable */
+    // eslint-disable-next-line
+    cy.wait(4000);
+    cy.get('[data-cy=newsletter-modal-popup]').should('exist');
+    cy.get('[data-cy=newsletter-modal-email-input]').type('test');
+    cy.get('[data-cy=newsletter-modal-subscribe-btn]').click();
+    cy.get('[data-cy=newsletter-modal-email-input]').type('@test.com');
+    cy.get('[data-cy=newsletter-modal-subscribe-btn]').click();
+    cy.get('[data-cy=newsletter-modal-popup]').should('not.exist');
   });
 });
