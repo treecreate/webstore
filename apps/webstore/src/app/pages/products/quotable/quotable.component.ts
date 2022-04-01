e900bb3bab29ee63c96cea3795c4e072f8a267ee
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-04-01 07:09:27 +0200

    refactor: 💡 Remove useless styling code lol

--- apps/webstore/src/app/pages/products/quotable/quotable.component.ts
+++ apps/webstore/src/app/pages/products/quotable/quotable.component.ts
@@ -13,18 +13,6 @@ import { ToastService } from '../../../shared/components/toast/toast-service';
 import { AuthService } from '../../../shared/services/authentication/auth.service';
 import { DesignService } from '../../../shared/services/design/design.service';
 
-const baconIpsum = [
-  'Tail leberkas boudin swine burgdoggen meatball pork loin chicken venison shankle shank beef spare ribs filet mignon tri-tip',
-  ' Turkey leberkas swine, meatball corned beef strip steak doner cow. Alcatra ribeye chicken prosciutto, leberkas salami pork belly shankle short loin',
-  '  Porchetta pancetta landjaeger, t-bone biltong beef boudin picanha chuck swine',
-  '  Filet mignon flank ribeye, beef short loin shankle swine kevin landjaeger sirloin buffalo short ribs alcatra',
-  '  Tri-tip turkey salami spare ribs, hamburger andouille pork loin chicken sirloin cow burgdoggen',
-  '  Leberkas doner jerky landjaeger, ham hock pork belly sirloin shoulder swine spare ribs ball tip',
-  '  Swine short loin pastrami doner, prosciutto hamburger pork spare ribs pork belly brisket chicken rump fatback meatloaf',
-  '  Tongue pork chop filet mignon shankle',
-  '  Sirloin bacon short loin pork, biltong turducken porchetta t-bone short ribs tongue',
-];
-
 @Component({
   selector: 'webstore-quotable',
   templateUrl: './quotable.component.html',
