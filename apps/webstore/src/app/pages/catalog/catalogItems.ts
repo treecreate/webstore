661059286ad94b69da4dbecdf97bfdd681f17ba0
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-04-02 11:14:17 +0200

    feat: 🎸 Optimize styling and add descriptions

--- apps/webstore/src/app/pages/catalog/catalogItems.ts
+++ apps/webstore/src/app/pages/catalog/catalogItems.ts
@@ -10,8 +10,8 @@ export interface CatalogItem {
 
 const familyTree: CatalogItem = {
   title: 'Family Tree',
-  descriptionEn: 'english description',
-  descriptionDk: 'danish description',
+  descriptionEn: 'Create your own family tree by adding the names of your family.',
+  descriptionDk: 'Lav dit eget stamtræ ved at skrive din families navne ind.',
   prices: [499, 699, 999],
   imgSrc: '/assets/img/product-display-img/family-tree.jpg',
   specialOffer: '30% off when purchasing 2',
@@ -20,21 +20,22 @@ const familyTree: CatalogItem = {
 
 const quotable: CatalogItem = {
   title: 'Quotable',
-  descriptionEn: 'english description',
-  descriptionDk: 'danish description',
+  descriptionEn: 'Select a frame and write a quote to someone you care about.',
+  descriptionDk: 'Vælg en ramme og skriv et citat til den du holder af.',
   prices: [349, 499, 699],
   imgSrc: '/assets/img/description2.jpg',
-  specialOffer: '30% off when purchasing 2',
+  specialOffer: '- 30% off when purchasing 2',
   routerLink: 'quotable',
 };
 
 const unique: CatalogItem = {
   title: 'Special order',
-  descriptionEn: 'english description',
-  descriptionDk: 'danish description',
+  descriptionEn:
+    'Send an image or describe what you want. We will see what we can create for you and give you a price.',
+  descriptionDk: 'Send et billede og bescrib hvad du vil have. We ser hvad vi kan gøre for dig og giver dig en pris.',
   prices: [],
   imgSrc: '/assets/img/description2.jpg',
-  specialOffer: 'Lets see what we can do for you',
+  specialOffer: '- Logo, Tegning, Billede eller noget helt andet.',
   routerLink: 'unique',
 };
 
