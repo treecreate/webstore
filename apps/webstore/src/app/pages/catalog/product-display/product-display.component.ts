34bdc94e0815bf332d7c304d4307357d0aae17ca
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-04-02 10:17:20 +0200

    feat: 🎸 Add localisation to products display

--- apps/webstore/src/app/pages/catalog/product-display/product-display.component.ts
+++ apps/webstore/src/app/pages/catalog/product-display/product-display.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { LocalStorageService } from '@local-storage';
+import { LocaleType, LocalStorageVars } from '@models';
+import { BehaviorSubject } from 'rxjs';
 import { CatalogItem } from '../catalogItems';
 
 @Component({
@@ -10,5 +13,15 @@ export class ProductDisplayComponent {
   @Input()
   product!: CatalogItem;
 
-  constructor() {}
+  locale$: BehaviorSubject<LocaleType>;
+  localeCode: LocaleType;
+
+  constructor(private localStorage: LocalStorageService) {
+    this.locale$ = this.localStorage.getItem<LocaleType>(LocalStorageVars.locale);
+    this.localeCode = this.locale$.getValue();
+  }
+
+  isEnglish(): boolean {
+    return this.localeCode === 'en-US';
+  }
 }
