4a506b71733ed08f62c2145b9d2a391139855e46
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-04-23 17:43:49 +0200

    feat: 🎸 Update home page for multi prod

--- apps/webstore/src/app/pages/products/products-items.constant.ts
+++ apps/webstore/src/app/pages/products/products-items.constant.ts
@@ -16,33 +16,33 @@ const familyTree: ProductsItem = {
   descriptionDk: 'Lav dit eget stamtræ ved at skrive din families navne ind.',
   prices: [499, 699, 999],
   imgSrc: '/assets/img/product-display-img/family-tree.jpg',
-  specialOffer: '30% off when purchasing 2',
+  specialOffer: '- 25% off when purchasing 4',
   routerLink: 'family-tree',
 };
 
 const quotable: ProductsItem = {
-  titleEn: 'Quote frame',
+  titleEn: 'Quotable',
   titleDk: 'Citatramme',
   descriptionEn: 'Select a frame and write a quote to someone you care about.',
-  descriptionDk: 'Vælg en ramme og skriv et citat til den du holder af.',
+  descriptionDk: 'Vælg en ramme og vælg et citat til den du holder af.',
   prices: [299, 399, 499],
   imgSrc: '/assets/img/description2.jpg',
-  specialOffer: '- 30% off when purchasing 2',
+  specialOffer: '- 25% off when purchasing 4',
   routerLink: 'quotable',
 };
 
-const unique: ProductsItem = {
-  titleEn: 'Custom order',
-  titleDk: 'Special bestilling',
-  descriptionEn:
-    'Send an image or describe what you want. We will see what we can create for you and give you a price.',
-  descriptionDk: 'Send et billede og bescrib hvad du vil have. We ser hvad vi kan gøre for dig og giver dig en pris.',
-  prices: [],
-  imgSrc: '/assets/img/description2.jpg',
-  specialOffer: '- Logo, Tegning, Billede eller noget helt andet.',
-  routerLink: 'unique',
-};
+// const unique: ProductsItem = {
+//   titleEn: 'Custom order',
+//   titleDk: 'Special bestilling',
+//   descriptionEn:
+//     'Send an image or describe what you want. We will see what we can create for you and give you a price.',
+//   descriptionDk: 'Send et billede og bescrib hvad du vil have. We ser hvad vi kan gøre for dig og giver dig en pris.',
+//   prices: [],
+//   imgSrc: '/assets/img/description2.jpg',
+//   specialOffer: '- Logo, Tegning, Billede eller noget helt andet.',
+//   routerLink: 'unique',
+// };
 
-const productsList: ProductsItem[] = [familyTree, quotable, unique];
+const productsList: ProductsItem[] = [quotable, familyTree];
 
 export default productsList;
