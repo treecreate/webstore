0d340b1fbf1f40270e872cc321d7b631a586f367
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-04-23 23:03:50 +0200

    feat: 🎸 Add quotable templates

--- apps/webstore/src/app/shared/components/modals/quotable-template-modal/quotable-template-modal.component.ts
+++ apps/webstore/src/app/shared/components/modals/quotable-template-modal/quotable-template-modal.component.ts
@@ -11,33 +11,33 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 export class QuotableTemplateModalComponent implements OnInit {
   templateList: IQuotableTemplate[] = [
     {
-      name: 'Til farmor',
-      text: 'Det eneste bedre \nend at have dig som \nmor\n\nEr at mine børn har dig\nsom Farmor',
+      name: 'Til Farmor/Farfar',
+      text: 'Det eneste bedre \nend at have dig som \nMOR/FAR\n\nEr at mine børn har dig\nsom FARMOR/FARFAR',
       fontSize: 20,
     },
     {
-      name: 'Til mor',
-      text: 'Jeg elsker dig mor,\nsom træer elsekr vand og solskin.\nDu hjælper mig med at vokse,\ntrives og nå nye højder.\n\nDin datter NAVN',
+      name: 'Til Mor/Far',
+      text: 'Jeg elsker dig MOR/FAR,\nsom træer elsekr vand og solskin.\nDu hjælper mig med at vokse,\ntrives og nå nye højder.\n\nDin SØN/DATTER\nNAVN',
       fontSize: 20,
     },
     {
-      name: 'Til Hende',
-      text: 'Smut sut',
+      name: 'Til Mor/Far',
+      text: 'MOR/FAR,\njeg vil gerne takke dig\nfor at have spillet en så utrolig\nrolle i mit liv.\n\nJeg elsker dig nu og for evigt.\nNAVN',
       fontSize: 20,
     },
     {
-      name: 'Til Ham',
-      text: 'Slik eller ik',
+      name: 'Til Ham/Hende',
+      text: 'Jeg skrev dit navn i skyerne,\nmen vinden blæste det væk.\n\nJeg skrev dit navn i sandet,\nmen bølgerne skyllede det væk.\n\nJeg skrev dit navn i mit hjerte,\nog for evigt vil det blive der.',
       fontSize: 20,
     },
     {
-      name: '',
-      text: '',
+      name: 'Barnedåb',
+      text: 'NAVN\n\n01.01.21 - kl.00.00\n\n4000g - 50cm',
       fontSize: 20,
     },
     {
-      name: '',
-      text: '',
+      name: 'Barnedåb 2',
+      text: 'NAVN\n\nFødsel: 01.01.21\nVægt: 4000g\nHøjde: 50cm\nKlokken: 00.00\nDåb: 01.01.21',
       fontSize: 20,
     },
   ];
