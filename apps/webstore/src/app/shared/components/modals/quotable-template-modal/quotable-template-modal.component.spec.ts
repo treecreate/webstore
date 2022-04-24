01fe3d0dbda384f30989f75eabc087f74a167894
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-04-24 11:29:22 +0200

    fix: 🐛 Fix some ci tests

--- apps/webstore/src/app/shared/components/modals/quotable-template-modal/quotable-template-modal.component.spec.ts
+++ apps/webstore/src/app/shared/components/modals/quotable-template-modal/quotable-template-modal.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { QuotableTemplateModalComponent } from './quotable-template-modal.component';
 
@@ -9,6 +11,8 @@ describe('QuotableTemplateModalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [QuotableTemplateModalComponent],
+      imports: [NgbModule, RouterTestingModule],
+      providers: [NgbActiveModal],
     }).compileComponents();
   });
 
