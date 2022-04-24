18f5946673661118dfb66c8c34615c32b9e0c875
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-04-24 17:09:01 +0200

    test: 💍 Add tests for quotable template modal

--- apps/webstore-e2e/src/integration/components/modals/template-quotable-modal.spec.ts
+++ apps/webstore-e2e/src/integration/components/modals/template-quotable-modal.spec.ts
@@ -7,10 +7,26 @@ describe('Template selection modal', () => {
   });
 
   it('opens when clicking select a template', () => {
-    cy.get('[data-cy=quotable-template-button]');
+    cy.get('[data-cy=quotable-template-button]').click({ force: true });
+    cy.get('[data-cy=quotable-template-modal]').should('exist');
   });
 
-  it('closes when clicking the x button', () => {});
+  it('closes when clicking the x button', () => {
+    cy.get('[data-cy=quotable-template-button]').click({ force: true });
+    cy.get('[data-cy=quotable-template-modal]').should('exist');
+    cy.get('[data-cy=quotable-template-modal-x-btn]').click({ force: true });
+    cy.get('[data-cy=quotable-template-modal]').should('not.exist');
+  });
 
-  it('selects the correct template', () => {});
+  it('selects the correct template', () => {
+    cy.get('[data-cy=quotable-template-button]').click({ force: true });
+    cy.get('[data-cy=quotable-template-modal]').should('exist');
+    cy.get('[data-cy=template-select-btn]').first().click({ force: true });
+    cy.get('[data-cy=save-button]').click({ force: true });
+    cy.visit('/products/quotable');
+    const localStorageDesignAfter = JSON.parse(localStorage.getItem(LocalStorageVars.designQuotable));
+    console.log(localStorageDesignAfter);
+
+    cy.get('[data-cy=text]').should('contain', 'Det eneste bedre');
+  });
 });
