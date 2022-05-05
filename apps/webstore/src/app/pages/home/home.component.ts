f1179b2b48181654581e6bedbe98b05dbc9b274f
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-05-05 14:48:26 +0200

    feat: 🎸 fix styling for review section

--- apps/webstore/src/app/pages/home/home.component.ts
+++ apps/webstore/src/app/pages/home/home.component.ts
@@ -21,24 +21,28 @@ export class HomeComponent implements OnInit {
   public localeCode: LocaleType;
   reviews = [
     {
+      id: 1,
       url: '',
       name: 'Maria Sotskova',
       textEn: `"I LOVE your beautiful way of perpetuating the family, and have gotten countless compliments on the beautiful family tree. You are so sweet to helpfull to your customers."`,
       textDk: `“Fantastisk fint og helt unikt produkt, som rammer lige i 'skabet'. Vi købte stamtræer til begge hold bedsteforældre som gaver, hvilket var en stor succes.”`,
     },
     {
+      id: 2,
       url: '',
       name: 'Stine Olsen',
       textEn: `"10/10 and more .. It gets praise every time we have guests, and many ask about it. Very nice product, top quality and we are more than satisfied."`,
       textDk: `“10/10! Og mere til .. det for ros hvergang vi har gæster, og mange spørg til det. Meget lækkert produkt, kvalitet i top, vi er mere end tilfredse.”`,
     },
     {
+      id: 3,
       url: '',
       name: 'Sabrine Dam',
       textEn: `"My family was incredibly happy for their family tree, i think that it is nice to have the family be part of the house decorations."`,
       textDk: `“Min familie blev utroligt glade for deres gave, jeg synes at det er fedt at kunne få familien med ind i indretning!”`,
     },
     {
+      id: 4,
       url: '',
       name: 'Birgitte Hansson',
       textEn: `"A fantastic and completely unique product, that is always on point. We bought the family tree for both pairs of grandparent as a gift, which was a big success"`,
