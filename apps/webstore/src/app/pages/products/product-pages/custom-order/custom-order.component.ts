19f711eb2a3286aa1371242f184e1014e4b44acf
Kwandes <bz.jasiek@gmail.com>
2022-07-25 20:52:31 +0200

    feat: 🎸 Add name, email and description fields

--- apps/webstore/src/app/pages/products/product-pages/custom-order/custom-order.component.ts
+++ apps/webstore/src/app/pages/products/product-pages/custom-order/custom-order.component.ts
@@ -1,8 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IAuthUser, IUser } from '@interfaces';
+import { LocalStorageService } from '@local-storage';
+import { LocalStorageVars } from '@models';
+import { BehaviorSubject } from 'rxjs';
+import { AuthService } from '../../../../shared/services/authentication/auth.service';
+import { OrderService } from '../../../../shared/services/order/order.service';
+import { UserService } from '../../../../shared/services/user/user.service';
 
 @Component({
   selector: 'webstore-custom-order',
   templateUrl: './custom-order.component.html',
-  styleUrls: ['./custom-order.component.scss', '../../../../../assets/styles/tc-input-field.scss'],
+  styleUrls: ['./custom-order.component.css', '../../../../../assets/styles/tc-input-field.scss'],
 })
-export class CustomOrderComponent {}
+export class CustomOrderComponent implements OnInit {
+  customOrderForm: FormGroup;
+
+  currentUser: IUser;
+  authUser$: BehaviorSubject<IAuthUser>;
+
+  isLoggedIn = false;
+  isLoading = false;
+  isImageRequirementsRead = false;
+
+  alert: {
+    type: 'success' | 'info' | 'warning' | 'danger';
+    message: string;
+    dismissible: boolean;
+  };
+
+  constructor(
+    private localStorageService: LocalStorageService,
+    private userService: UserService,
+    private authService: AuthService,
+    private orderService: OrderService
+  ) {
+    // Listen to changes to login status
+    this.authUser$ = this.localStorageService.getItem<IAuthUser>(LocalStorageVars.authUser);
+    this.authUser$.subscribe(() => {
+      // Check if the access token is still valid
+      this.isLoggedIn = this.authUser$.getValue() != null && this.authService.isAccessTokenValid();
+    });
+  }
+
+  ngOnInit(): void {
+    this.initForms();
+    if (this.isLoggedIn) {
+      //Get user and update form
+      this.userService.getUser().subscribe((user: IUser) => {
+        this.currentUser = user;
+        this.updateFormValues();
+      });
+    }
+  }
+
+  initForms() {
+    this.customOrderForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.maxLength(50),
+        Validators.minLength(3),
+        Validators.required,
+        Validators.pattern('^[^0-9]+$'),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      description: new FormControl('', [Validators.maxLength(1000), Validators.minLength(1), Validators.required]),
+    });
+  }
+
+  updateFormValues() {
+    this.customOrderForm.setValue({
+      email: this.currentUser.email,
+    });
+  }
+
+  submitCustomOrder() {
+    this.createCustomOrder();
+  }
+
+  isDisabled() {
+    return this.isImageRequirementsRead && this.customOrderForm.valid;
+  }
+
+  createCustomOrder() {
+    if (!this.isDisabled()) {
+      console.warn('You are not able to create a custom order without valid information');
+      return;
+    }
+    this.isLoading = true;
+    // TODO Add the API call via ordersService
+    console.log('Request submitted');
+  }
+}
