c3d4d440a495b2f2a3dbf9889df5068e092343ff
Kwandes <bz.jasiek@gmail.com>
2022-07-27 21:14:39 +0200

    fix: 🐛 Add somehow uncomitted DTO

---
+++ apps/api/src/main/java/dk/treecreate/api/events/dto/CreateEventRequest.java
@@ -0,0 +1,41 @@
+package dk.treecreate.api.events.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.UUID;
+
+public class CreateEventRequest
+{
+    @NotBlank
+    @Size(max = 50)
+    @ApiModelProperty(example = "webstore.example.com", required = true)
+    private String name;
+
+    @NotNull
+    @ApiModelProperty(notes = "userId associated with this event",
+        example = "c0a80121-7ac0-190b-817a-c08ab0a12345", required = true)
+    private UUID userId;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public UUID getUserId()
+    {
+        return userId;
+    }
+
+    public void setUserId(UUID userId)
+    {
+        this.userId = userId;
+    }
+}
