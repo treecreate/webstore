b2497f5d76f25e1c174c0c2397c29eb1c668d833
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-08-04 00:30:13 +0200

    style: 💄 Apply formatting

--- apps/webstore/src/app/pages/products/product-pages/custom-order/custom-order.component.ts
+++ apps/webstore/src/app/pages/products/product-pages/custom-order/custom-order.component.ts
@@ -30,40 +30,32 @@ export class CustomOrderComponent implements OnInit {
     },
     {
       customer: 'We Do Agency',
-      descriptionEn:
-        '4 unique prices at a BMW event, where the prises gifted to the winners.',
-      descriptionDk:
-        '4 personlige præmier til et BMW event, hvor prierne blev uddelt til vinderne. ',
+      descriptionEn: '4 unique prices at a BMW event, where the prises gifted to the winners.',
+      descriptionDk: '4 personlige præmier til et BMW event, hvor prierne blev uddelt til vinderne. ',
       price: 1400,
       altText: '',
       imgSrc: '/assets/img/custom-order-example/custom-order-img-3.png',
     },
     {
       customer: 'Jakobsen',
-      descriptionEn:
-        'A beautiful display in the memory of the family\'s golden retriever named Bella.',
-      descriptionDk:
-        'En flot tavle i minde om familiens golden retriever ved navn Bella.',
+      descriptionEn: "A beautiful display in the memory of the family's golden retriever named Bella.",
+      descriptionDk: 'En flot tavle i minde om familiens golden retriever ved navn Bella.',
       price: 400,
       altText: '',
       imgSrc: '/assets/img/custom-order-example/custom-order-img-1.png',
     },
     {
       customer: 'Helene',
-      descriptionEn:
-        'A gift from Helene to her boyfriend for their 2 year anniversary.',
-      descriptionDk:
-        'En gave fra Helene til hendes kæreste til deres 2 års dag sammen.',
+      descriptionEn: 'A gift from Helene to her boyfriend for their 2 year anniversary.',
+      descriptionDk: 'En gave fra Helene til hendes kæreste til deres 2 års dag sammen.',
       price: 700,
       altText: '',
       imgSrc: '/assets/img/custom-order-example/custom-order-img-4.png',
     },
     {
       customer: 'Sara',
-      descriptionEn:
-        'A display for Saras dog named Buster, with the addition of some puppy paws by the name.',
-      descriptionDk:
-        'En smuk tavle for Saras hund ved navn Buster. Navnet buster fik nogle hundepoter over sit navn.',
+      descriptionEn: 'A display for Saras dog named Buster, with the addition of some puppy paws by the name.',
+      descriptionDk: 'En smuk tavle for Saras hund ved navn Buster. Navnet buster fik nogle hundepoter over sit navn.',
       price: 400,
       altText: '',
       imgSrc: '/assets/img/custom-order-example/custom-order-img-5.png',
