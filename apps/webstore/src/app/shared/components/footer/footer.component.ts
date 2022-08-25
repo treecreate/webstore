69600948396f74339796795ca0868d367f119fa3
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-08-25 23:57:52 +0200

    feat: 🎸 Add autofocus on newsletter signup in footer

--- apps/webstore/src/app/shared/components/footer/footer.component.ts
+++ apps/webstore/src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { ErrorlogPriorityEnum, IAuthUser, INewsletter } from '@interfaces';
 import { LocalStorageService } from '@local-storage';
@@ -25,6 +25,7 @@ export class FooterComponent implements OnInit {
   public currentYear = new Date().getFullYear();
   signupNewsletterForm: UntypedFormGroup;
   isLoading = false;
+  @ViewChild('emailInput') emailInput: ElementRef;
 
   constructor(
     private modalService: NgbModal,
@@ -51,24 +52,30 @@ export class FooterComponent implements OnInit {
 
   submit() {
     this.isLoading = true;
-    this.newsletterService.registerNewsletterEmail(this.signupNewsletterForm.get('email').value).subscribe(
-      (newsletterData: INewsletter) => {
-        this.toastService.showAlert(
-          `Thank you for subscribing: ${newsletterData.email}`,
-          `Tak for din tilmelding: ${newsletterData.email}`,
-          'success',
-          3000
-        );
-        this.isLoading = false;
-        this.eventsService.create('webstore.footer.newsletter-signup');
-      },
-      (error) => {
-        console.error(error);
-        this.errorlogsService.create('webstore.footer.newsletter-signup-failed', ErrorlogPriorityEnum.high, error);
-        this.toastService.showAlert(error.error.message, error.error.message, 'danger', 100000);
-        this.isLoading = false;
-      }
-    );
+    if (this.signupNewsletterForm.get('email').value !== '') {
+      this.newsletterService.registerNewsletterEmail(this.signupNewsletterForm.get('email').value).subscribe(
+        (newsletterData: INewsletter) => {
+          this.toastService.showAlert(
+            `Thank you for subscribing: ${newsletterData.email}`,
+            `Tak for din tilmelding: ${newsletterData.email}`,
+            'success',
+            3000
+          );
+          this.isLoading = false;
+          this.eventsService.create('webstore.footer.newsletter-signup');
+        },
+        (error) => {
+          console.error(error);
+          this.errorlogsService.create('webstore.footer.newsletter-signup-failed', ErrorlogPriorityEnum.high, error);
+          this.toastService.showAlert('Invalid email', 'Ugyldig email', 'danger', 100000);
+          this.isLoading = false;
+        }
+      );
+    } else {
+      this.toastService.showAlert('Missing email input.', 'Udfyld email feltet.', 'danger', 100000);
+      this.emailInput.nativeElement.focus();
+      this.isLoading = false;
+    }
   }
 
   showTermsOfUse() {
