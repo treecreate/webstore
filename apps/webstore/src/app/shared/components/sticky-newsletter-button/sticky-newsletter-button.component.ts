2f76be442ff1b87e5ce202fcc3d61f1ef20daf59
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-08-31 19:55:59 +0200

    fix: 🐛 values title overlap fix

--- apps/webstore/src/app/shared/components/sticky-newsletter-button/sticky-newsletter-button.component.ts
+++ apps/webstore/src/app/shared/components/sticky-newsletter-button/sticky-newsletter-button.component.ts
@@ -14,16 +14,11 @@ import { NewsletterSignupModalComponent } from '../modals/newsletter-signup-moda
   styleUrls: ['./sticky-newsletter-button.component.scss'],
 })
 export class StickyNewsletterButtonComponent {
-  
   private authUser$: BehaviorSubject<IAuthUser>;
-  public isSubscribed: Boolean = true; 
+  public isSubscribed: Boolean = true;
   public isLoggedIn = false;
 
-  constructor(
-    private modalService: NgbModal,
-  ) {
-    
-  }
+  constructor(private modalService: NgbModal) {}
 
   openNewsletterModal() {
     this.modalService.open(NewsletterSignupModalComponent);
