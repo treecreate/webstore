70d54bbad15cbb838b3efb9c21e7bb9031b6c154
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-09-01 14:25:53 +0200

    feat: 🎸 Add styling and check if cookies have been accepted

--- apps/webstore/src/app/shared/components/sticky-newsletter-button/sticky-newsletter-button.component.ts
+++ apps/webstore/src/app/shared/components/sticky-newsletter-button/sticky-newsletter-button.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from '@local-storage';
+import { CookieStatus, LocalStorageVars } from '@models';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { NewsletterSignupModalComponent } from '../modals/newsletter-signup-modal/newsletter-signup-modal.component';
 
@@ -10,8 +12,24 @@ import { NewsletterSignupModalComponent } from '../modals/newsletter-signup-moda
 export class StickyNewsletterButtonComponent {
   public isSubscribed = true;
   public isLoggedIn = false;
+  public isCookiePromptShow = false;
+  private cookiePrompt = document.getElementById('cookiePromptModal');
+  public cookiePromptHeight: number;
 
-  constructor(private modalService: NgbModal) {}
+  constructor(private modalService: NgbModal, private localStorageService: LocalStorageService) {
+    // see if cookies have been accepted/denied
+    this.localStorageService.getItem(LocalStorageVars.cookiesAccepted).subscribe(() => {
+      if (!this.localStorageService.getItem(LocalStorageVars.cookiesAccepted).value !== undefined) {
+        console.log('not accepted');
+        this.isCookiePromptShow = true;
+        this.cookiePromptHeight = this.cookiePrompt.offsetHeight + 16;
+      } else {
+        console.log('accepted');
+        this.isCookiePromptShow = false;
+        this.cookiePromptHeight = 16;
+      }
+    });
+  }
 
   openNewsletterModal() {
     this.modalService.open(NewsletterSignupModalComponent);
