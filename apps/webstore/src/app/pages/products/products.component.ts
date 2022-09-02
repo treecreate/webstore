44e33199c51a09cee6e0711176a81aa3dee2f852
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-09-02 21:16:19 +0200

    feat: 🎸 Add dynamic metadata to products page

--- apps/webstore/src/app/pages/products/products.component.ts
+++ apps/webstore/src/app/pages/products/products.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 import { LocalStorageService } from '@local-storage';
 import { LocaleType, LocalStorageVars } from '@models';
 import productsItems, { ProductsItem } from './products-items.constant';
@@ -8,13 +9,23 @@ import productsItems, { ProductsItem } from './products-items.constant';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
   productsList: ProductsItem[] = productsItems;
 
   localeCode: LocaleType;
 
-  constructor(private localStorage: LocalStorageService) {
+  constructor(private localStorage: LocalStorageService, private metaTitle: Title, private meta: Meta) {
     this.localeCode = this.localStorage.getItem<LocaleType>(LocalStorageVars.locale).value;
+  }
+
+  ngOnInit(): void {
+    this.setMetaData();
+  }
+
+  setMetaData() {
+    this.metaTitle.setTitle('');
+    this.meta.updateTag({ name: 'description', content: '' });
+    this.meta.updateTag({ name: 'keywords', content: '' });
   }
 
   isEnglish(): boolean {
