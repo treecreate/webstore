d54bc5dcf3d12768ace349f7cee48826abc02321
Kwandes <bz.jasiek@gmail.com>
2022-09-03 00:14:51 +0200

    test: 💍 Adjust e2e tests for localization

--- apps/webstore-e2e/src/integration/services/localization/localization.spec.ts
+++ apps/webstore-e2e/src/integration/services/localization/localization.spec.ts
@@ -4,6 +4,7 @@ describe('Localization', () => {
   });
 
   it('change localized to danish', () => {
+    cy.visit('/');
     // activate dropdownmenu with languages
     cy.get('[data-cy=navbar-localization]').trigger('mouseenter');
     cy.get('[data-cy=navbar-localization-dk]').click();
@@ -13,6 +14,7 @@ describe('Localization', () => {
   });
 
   it('change localized to english', () => {
+    cy.visit('/');
     // activate dropdownmenu with languages
     cy.get('[data-cy=navbar-localization]').trigger('mouseenter');
     cy.get('[data-cy=navbar-localization-en]').click();
