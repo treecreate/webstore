9e15aa22290ae28a92bdb9318cf11c68fc5d866d
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-09-02 21:11:47 +0200

    feat: 🎸 Add dynamic metadata to home

--- apps/webstore/src/app/pages/home/home.component.ts
+++ apps/webstore/src/app/pages/home/home.component.ts
@@ -5,6 +5,7 @@ import { BehaviorSubject } from 'rxjs';
 import { LocalStorageService } from '@local-storage';
 import { NewsletterSignupModalComponent } from '../../shared/components/modals/newsletter-signup-modal/newsletter-signup-modal.component';
 import { reviewList } from './reviews.constant';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'webstore-home',
@@ -21,13 +22,20 @@ export class HomeComponent implements OnInit {
 
   title = 'homeComponent';
 
-  constructor(private localStorageService: LocalStorageService, private modalService: NgbModal) {
+  constructor(
+    private localStorageService: LocalStorageService,
+    private modalService: NgbModal,
+    private metaTitle: Title,
+    private meta: Meta
+  ) {
     // Listen to changes to locale
     this.locale$ = this.localStorageService.getItem<LocaleType>(LocalStorageVars.locale);
     this.localeCode = this.locale$.getValue();
   }
 
   ngOnInit(): void {
+    this.setMetaData();
+
     const hasSeenNewsletterModal = this.localStorageService.getItem<boolean>(LocalStorageVars.hasSeenNewsletterModal);
     if (!hasSeenNewsletterModal.value) {
       setTimeout(() => {
@@ -35,6 +43,12 @@ export class HomeComponent implements OnInit {
         this.localStorageService.setItem<boolean>(LocalStorageVars.hasSeenNewsletterModal, true);
       }, 5000);
     }
+  }
+
+  setMetaData() {
+    this.metaTitle.setTitle('');
+    this.meta.updateTag({ name: 'description', content: '' });
+    this.meta.updateTag({ name: 'keywords', content: '' });
   }
 
   isEnglish(): boolean {
