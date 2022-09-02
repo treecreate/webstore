1a7ddde3a1b0ef54e8de1506a27718bc54090c4c
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-09-02 11:46:25 +0200

    feat: 🎸 Add 2 sec timeout before showing cookies

--- apps/webstore/src/app/app.component.ts
+++ apps/webstore/src/app/app.component.ts
@@ -18,6 +18,7 @@ declare let gtag: Function;
 })
 export class AppComponent {
   title = 'Treecreate';
+  showCookiePrompt = false;
 
   constructor(
     public router: Router,
@@ -64,6 +65,10 @@ export class AppComponent {
       window['prerenderReady'] = true;
       document.getElementById('prerenderScriptTag').innerHTML = 'window.prerenderReady = true';
     }, 5000);
+
+    setTimeout(() => {
+      this.showCookiePrompt = true;
+    }, 2000);
   }
 
   initGoogleAnalytics() {
