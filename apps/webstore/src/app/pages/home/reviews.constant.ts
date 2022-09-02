f1bb6309754167b6c0f056d4d409122f445a2510
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-09-02 20:39:28 +0200

    refactor: 💡 move reviews to const file

---
+++ apps/webstore/src/app/pages/home/reviews.constant.ts
@@ -0,0 +1,34 @@
+export const reviewList = [
+  {
+    id: 1,
+    alt: '3 personlige gaver',
+    url: '/assets/img/review-img/review-img-04.jpg',
+    name: 'Maria',
+    textEn: `"I LOVE your beautiful way of perpetuating the family, and have gotten countless compliments on the beautiful family tree. You are so sweet to helpfull to your customers."`,
+    textDk: `“Fantastisk fint og helt unikt produkt, som rammer lige i 'skabet'. Vi købte stamtræer til begge hold bedsteforældre som gaver, hvilket var en stor succes.”`,
+  },
+  {
+    id: 2,
+    alt: 'En gave til farmor',
+    url: '/assets/img/review-img/review-img-01.jpg',
+    name: 'Stine',
+    textEn: `"10/10 and more .. It gets praise every time we have guests, and many ask about it. Very nice product, top quality and we are more than satisfied."`,
+    textDk: `“10/10! Og mere til .. det får ros hver gang vi har gæster, og mange spørger ind til det. Meget lækkert produkt, kvalitet i top, vi er mere end tilfredse.”`,
+  },
+  {
+    id: 3,
+    alt: 'En gave til mor',
+    url: '/assets/img/review-img/review-img-03.jpg',
+    name: 'Sabrine',
+    textEn: `"My family was incredibly happy for their family tree, i think that it is nice to have the family be part of the house decorations."`,
+    textDk: `“Min familie blev utroligt glade for deres gave, jeg synes at det er fedt at kunne få familien med ind i indretning!”`,
+  },
+  {
+    id: 4,
+    alt: 'Familien samlet om gaven',
+    url: '/assets/img/review-img/review-img-02.jpg',
+    name: 'Birgitte',
+    textEn: `"A fantastic and completely unique product, that is always on point. We bought the family tree for both pairs of grandparent as a gift, which was a big success"`,
+    textDk: `“Fantastisk fint og helt unikt produkt, som rammer lige i 'skabet'. Vi købte stamtræer til begge hold bedsteforældre som gaver, hvilket var en stor succes.”`,
+  },
+];
