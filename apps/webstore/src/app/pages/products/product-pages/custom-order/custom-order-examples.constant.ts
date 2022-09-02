085fb0dd8f79b16dd230e4f498a4cd7f8d0d1a9b
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-09-02 20:40:42 +0200

    refactor: 💡 move customOrderExamples to const file

---
+++ apps/webstore/src/app/pages/products/product-pages/custom-order/custom-order-examples.constant.ts
@@ -0,0 +1,54 @@
+export const customOrderExamples = [
+  {
+    customer: 'Penneo',
+    descriptionEn:
+      'A unique display of the Penneo logo combined with the Nasdaq name in celebration of being listed on the main market.',
+    descriptionDk:
+      'Et unik plade med Penneos logo på, kombineret med Nasdaq navnet. Lavet i en fejring af at blive en del af Nasdaq-indexet.',
+    price: 700,
+    altText: '',
+    imgSrc: '/assets/img/custom-order-example/custom-order-img-2.png',
+  },
+  {
+    customer: 'We Do Agency',
+    descriptionEn: '4 unique prices at a BMW event, where the prises gifted to the winners.',
+    descriptionDk: '4 personlige præmier til et BMW event, hvor prierne blev uddelt til vinderne. ',
+    price: 1400,
+    altText: '',
+    imgSrc: '/assets/img/custom-order-example/custom-order-img-3.png',
+  },
+  {
+    customer: 'Jakobsen',
+    descriptionEn: "A beautiful display in the memory of the family's golden retriever named Bella.",
+    descriptionDk: 'En flot tavle i minde om familiens golden retriever ved navn Bella.',
+    price: 400,
+    altText: '',
+    imgSrc: '/assets/img/custom-order-example/custom-order-img-1.png',
+  },
+  {
+    customer: 'Helene',
+    descriptionEn: 'A gift from Helene to her boyfriend for their 2 year anniversary.',
+    descriptionDk: 'En gave fra Helene til hendes kæreste til deres 2 års dag sammen.',
+    price: 700,
+    altText: '',
+    imgSrc: '/assets/img/custom-order-example/custom-order-img-4.png',
+  },
+  {
+    customer: 'Sara',
+    descriptionEn: 'A display for Saras dog named Buster, with the addition of some puppy paws by the name.',
+    descriptionDk: 'En smuk tavle for Saras hund ved navn Buster. Navnet buster fik nogle hundepoter over sit navn.',
+    price: 400,
+    altText: '',
+    imgSrc: '/assets/img/custom-order-example/custom-order-img-5.png',
+  },
+  {
+    customer: 'Treecreate',
+    descriptionEn:
+      'A charming, sustainable and beautiful high-quality wooden keychain. Perfect for your company’s employees.',
+    descriptionDk:
+      'En charmerende, bæredygtig og smuk nøglering i træ i høj kvalitet. Helt perfekt til din virksomheds medarbejdere.',
+    price: 400,
+    altText: '',
+    imgSrc: '/assets/img/description2.jpg',
+  },
+];
