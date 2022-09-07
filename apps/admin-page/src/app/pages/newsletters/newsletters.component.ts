3b074d870078fca38080c7507b6e9975322eac41
Dominic Smith <Dominic_smith2012@hotmail.co.uk>
2022-09-07 09:29:13 +0200

    feat: 🎸 Reversed the order of the sorting as requested

--- apps/admin-page/src/app/pages/newsletters/newsletters.component.ts
+++ apps/admin-page/src/app/pages/newsletters/newsletters.component.ts
@@ -34,7 +34,7 @@ export class NewslettersComponent {
     this.isLoading = true;
     this.newsletterService.getNewsletters().subscribe({
       next: (response: INewsletter[]) => {
-        this.newsletterList = response.sort((a, b) => a.createdAt.localeCompare(b.createdAt));
+        this.newsletterList = response.sort((a, b) => a.createdAt.localeCompare(b.createdAt)).reverse();
         this.isLoading = false;
         this.setTableInfo();
       },
