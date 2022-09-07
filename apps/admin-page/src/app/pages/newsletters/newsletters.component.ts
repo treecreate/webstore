b6311e36c3536316175b5783268e1672346a692d
Dominic Smith <Dominic_smith2012@hotmail.co.uk>
2022-09-07 09:10:49 +0200

    feat: 🎸 Sorted the newsletter array by date

--- apps/admin-page/src/app/pages/newsletters/newsletters.component.ts
+++ apps/admin-page/src/app/pages/newsletters/newsletters.component.ts
@@ -34,7 +34,7 @@ export class NewslettersComponent {
     this.isLoading = true;
     this.newsletterService.getNewsletters().subscribe({
       next: (response: INewsletter[]) => {
-        this.newsletterList = response;
+        this.newsletterList = response.sort((a, b) => a.createdAt.localeCompare(b.createdAt));
         this.isLoading = false;
         this.setTableInfo();
       },
