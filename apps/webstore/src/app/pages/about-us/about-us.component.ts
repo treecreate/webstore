f45127a2aaa89c81e6f10a0d80e92966eca6962a
Kwandes <bz.jasiek@gmail.com>
2022-09-12 00:53:33 +0200

    feat: 🎸 Add logic for newsletter signup

--- apps/webstore/src/app/pages/about-us/about-us.component.ts
+++ apps/webstore/src/app/pages/about-us/about-us.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { ErrorlogPriorityEnum, INewsletter } from '@interfaces';
+import { ToastService } from '../../shared/components/toast/toast-service';
+import { ErrorlogsService } from '../../shared/services/errorlog/errorlog.service';
+import { EventsService } from '../../shared/services/events/events.service';
 import { NewsletterService } from '../../shared/services/newsletter/newsletter.service';
 
 @Component({
@@ -11,16 +15,45 @@ export class AboutUsComponent {
   treesPlanted = 120; // TODO - fetch the planted trees from the API
   newsletterForm: UntypedFormGroup;
   isLoading = false;
+  @ViewChild('emailInput') emailInput: ElementRef;
 
-  constructor(private newsletterService: NewsletterService) {
+  constructor(
+    private newsletterService: NewsletterService,
+    private toastService: ToastService,
+    private eventsService: EventsService,
+    private errorlogsService: ErrorlogsService
+  ) {
     this.newsletterForm = new UntypedFormGroup({
       email: new UntypedFormControl('', [Validators.required, Validators.email]),
     });
   }
 
   registerNewsletter(): void {
-    //TODO - Implement newsletter signup on the about us page
-    console.warn('WIP - Newsletter signup on about us page is not implemented yet!');
+    this.isLoading = true;
+    if (this.newsletterForm.get('email').value !== '') {
+      this.newsletterService.registerNewsletterEmail(this.newsletterForm.get('email').value).subscribe(
+        (newsletterData: INewsletter) => {
+          this.toastService.showAlert(
+            `Thank you for subscribing: ${newsletterData.email}`,
+            `Tak for din tilmelding: ${newsletterData.email}`,
+            'success',
+            3000
+          );
+          this.isLoading = false;
+          this.eventsService.create('webstore.about-us.newsletter-signup');
+        },
+        (error) => {
+          console.error(error);
+          this.errorlogsService.create('webstore.about-us.newsletter-signup-failed', ErrorlogPriorityEnum.high, error);
+          this.toastService.showAlert('Invalid email', 'Ugyldig email', 'danger', 5000);
+          this.isLoading = false;
+        }
+      );
+    } else {
+      this.toastService.showAlert('Missing email input.', 'Udfyld email feltet.', 'danger', 5000);
+      this.emailInput.nativeElement.focus();
+      this.isLoading = false;
+    }
   }
 
   isNewsletterReadyForSignup(): boolean {
