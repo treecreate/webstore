2f1e68cc5d641a6c4d5639a13eaf75603c034f9c
Kwandes <bz.jasiek@gmail.com>
2022-09-12 00:40:26 +0200

    feat: 🎸 Add "newsletter registration" section

--- apps/webstore/src/app/pages/about-us/about-us.component.ts
+++ apps/webstore/src/app/pages/about-us/about-us.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { NewsletterService } from '../../shared/services/newsletter/newsletter.service';
 
 @Component({
   selector: 'webstore-about-us',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AboutUsComponent {
   treesPlanted = 120; // TODO - fetch the planted trees from the API
+  newsletterForm: UntypedFormGroup;
+  isLoading = false;
+
+  constructor(private newsletterService: NewsletterService) {
+    this.newsletterForm = new UntypedFormGroup({
+      email: new UntypedFormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  registerNewsletter(): void {
+    //TODO - Implement newsletter signup on the about us page
+    console.warn('WIP - Newsletter signup on about us page is not implemented yet!');
+  }
+
+  isNewsletterReadyForSignup(): boolean {
+    return this.newsletterForm.dirty && this.newsletterForm.valid && !this.isLoading;
+  }
 }
