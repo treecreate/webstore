b9f5f4f1f5d206e6712466219902970bdb8f8c2c
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-09-29 15:06:22 +0200

    feat: 🎸 Add new lib enums

---
+++ libs/assets/src/lib/quotable/love-letter/love-letter.enum.ts
@@ -0,0 +1,26 @@
+export enum LoveLetterDesignEnum {
+  noFrame = 'assets/quotable/love-letter/frame-design/frame0-no-design.svg',
+  frame1 = 'assets/quotable/love-letter/frame-design/frame1.svg',
+  frame2 = 'assets/quotable/love-letter/frame-design/frame2.svg',
+  frame3 = 'assets/quotable/love-letter/frame-design/frame3.svg',
+  frame4 = 'assets/quotable/love-letter/frame-design/frame4.svg',
+  frame5 = 'assets/quotable/love-letter/frame-design/frame5.svg',
+}
+
+export enum LoveLetterDesignNameDanishEnum {
+  noFrame = 'Ingen Ramme',
+  frame1 = 'Simpel Stil',
+  frame2 = 'Frugt Ramme',
+  frame3 = 'Simpel Klassisk',
+  frame4 = 'Klassisk U',
+  frame5 = 'Klassisk OU',
+}
+
+export enum LoveLetterDesignNameEnglishEnum {
+  noFrame = 'No Frame',
+  frame1 = 'Simple Style',
+  frame2 = 'Fruit Frame',
+  frame3 = 'Simple Classic',
+  frame4 = 'Classic U',
+  frame5 = 'Classic UO',
+}
