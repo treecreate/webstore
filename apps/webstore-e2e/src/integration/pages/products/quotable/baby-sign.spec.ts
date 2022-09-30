3f35c0b1122f793f7b2159acdf0d3bdd6b983748
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-09-30 20:17:45 +0200

    feat: 🎸 e2e tests and refactor

---
+++ apps/webstore-e2e/src/integration/pages/products/quotable/baby-sign.spec.ts
@@ -0,0 +1,63 @@
+import { QuotableDesignEnum } from '@assets';
+import { DesignFontEnum, DesignTypeEnum, IDesign, IQoutable } from '@interfaces';
+import { CookieStatus, LocalStorageVars } from '@models';
+
+const quotableDesing: IQoutable = {
+  font: DesignFontEnum.archaMedium,
+  fontSize: 40,
+  designSrc: QuotableDesignEnum.frame18,
+  text: 'skrt skrt',
+};
+const mockQuotableDesign: IDesign = {
+  designId: 'MOCK_ID',
+  designProperties: quotableDesing,
+  mutable: true,
+  user: null,
+  designType: DesignTypeEnum.quotable,
+};
+
+describe('BabySignProductPage', () => {
+  beforeEach(() => {
+    localStorage.setItem(LocalStorageVars.cookiesAccepted, `"${CookieStatus.accepted}"`);
+    localStorage.setItem(LocalStorageVars.firstVisit, 'true');
+    cy.visit('/products/quotable?productType=BABY_SIGN');
+  });
+
+  describe('Baby sign product page', () => {
+    it('has the right frames', () => {
+
+    });
+
+    it('goes to the right frame when pressing prev', () => {
+      cy.get('[data-cy=design]').should('contain', 'assets/quotable/frame-design/frame18.svg');
+
+      cy.get('[data-cy=prev-design-button]').click({ force: true });
+      cy.get('[data-cy=design]').should('contain', 'assets/quotable/frame-design/frame0-no-design.svg');
+
+      cy.get('[data-cy=prev-design-button]').click({ force: true });
+      cy.get('[data-cy=design]').should('contain', 'assets/quotable/frame-design/frame22.svg');
+
+      cy.get('[data-cy=prev-design-button]').click({ force: true });
+      cy.get('[data-cy=design]').should('contain', 'assets/quotable/frame-design/frame21.svg');
+    });
+
+    it('goes to the right frame when pressing next', () => {
+      cy.get('[data-cy=design]').should('contain', 'assets/quotable/frame-design/frame18.svg');
+
+      cy.get('[data-cy=next-design-button]').click({ force: true });
+      cy.get('[data-cy=design]').should('contain', 'assets/quotable/frame-design/frame19.svg');
+
+      cy.get('[data-cy=next-design-button]').click({ force: true });
+      cy.get('[data-cy=design]').should('contain', 'assets/quotable/frame-design/frame20.svg');
+
+      cy.get('[data-cy=next-design-button]').click({ force: true });
+      cy.get('[data-cy=design]').should('contain', 'assets/quotable/frame-design/frame21.svg');
+
+      cy.get('[data-cy=next-design-button]').click({ force: true });
+      cy.get('[data-cy=design]').should('contain', 'assets/quotable/frame-design/frame22.svg');
+
+      cy.get('[data-cy=next-design-button]').click({ force: true });
+      cy.get('[data-cy=design]').should('contain', 'assets/quotable/frame-design/frame0-no-design.svg');
+    });
+  });
+});
