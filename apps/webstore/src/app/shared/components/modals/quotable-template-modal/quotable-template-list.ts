ab0ebda87b2ac03ec7fff926c3b6b92e981f960a
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-10-07 14:27:16 +0200

    refactor: 💡 Move templateList into file

---
+++ apps/webstore/src/app/shared/components/modals/quotable-template-modal/quotable-template-list.ts
@@ -0,0 +1,52 @@
+import { IQuotableTemplate } from '@interfaces';
+
+export const quotableTemplateList: IQuotableTemplate[] = [
+  {
+    name: 'Til Farmor/Farfar',
+    text: 'Det eneste bedre \nend at have dig som \nMOR/FAR\n\nEr at mine børn har dig\nsom FARMOR/FARFAR',
+    showText: true,
+    showTitle: false,
+    title: '',
+    fontSize: 20,
+  },
+  {
+    name: 'Til Mor/Far',
+    text: 'Jeg elsker dig MOR/FAR,\nsom træer elsker vand og solskin.\nDu hjælper mig med at vokse,\ntrives og nå nye højder.\n\nDin SØN/DATTER\nNAVN',
+    showText: true,
+    showTitle: false,
+    title: '',
+    fontSize: 20,
+  },
+  {
+    name: 'Til Mor/Far',
+    text: 'MOR/FAR,\njeg vil gerne takke dig\nfor at have spillet en så utrolig\nrolle i mit liv.\n\nJeg elsker dig nu og for evigt.\nNAVN',
+    showText: true,
+    showTitle: false,
+    title: '',
+    fontSize: 20,
+  },
+  {
+    name: 'Til Ham/Hende',
+    text: 'Jeg skrev dit navn i skyerne,\nmen vinden blæste det væk.\n\nJeg skrev dit navn i sandet,\nmen bølgerne skyllede det væk.\n\nJeg skrev dit navn i mit hjerte,\nog for evigt vil det blive der.',
+    showText: true,
+    showTitle: false,
+    title: '',
+    fontSize: 20,
+  },
+  {
+    name: 'Barnedåb',
+    text: 'NAVN\n\n01.01.21 - kl.00.00\n\n4000g - 50cm',
+    showText: true,
+    showTitle: false,
+    title: '',
+    fontSize: 20,
+  },
+  {
+    name: 'Barnedåb 2',
+    text: 'NAVN\n\nFødsel: 01.01.21\nVægt: 4000g\nHøjde: 50cm\nKlokken: 00.00\nDåb: 01.01.21',
+    showText: true,
+    showTitle: false,
+    title: 'hva så derrrr',
+    fontSize: 20,
+  },
+];
