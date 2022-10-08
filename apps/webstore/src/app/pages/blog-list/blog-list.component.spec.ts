60ad0791a4b16707e6e17f227fa9e6e4e9067872
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-10-08 15:07:21 +0200

    feat: 🎸 Initiate blog-list page

---
+++ apps/webstore/src/app/pages/blog-list/blog-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BlogListComponent } from './blog-list.component';
+
+describe('BlogListComponent', () => {
+  let component: BlogListComponent;
+  let fixture: ComponentFixture<BlogListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BlogListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BlogListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
