9dcb801070d098af2385f5114a953a5c5b248da3
Kwandes <bz.jasiek@gmail.com>
2022-10-21 12:29:33 +0200

    refactor: 💡 Clean up code, fix typos

--- apps/webstore/src/app/shared/components/products/pet-sign-design/pet-sign-design.component.ts
+++ apps/webstore/src/app/shared/components/products/pet-sign-design/pet-sign-design.component.ts
@@ -74,14 +74,6 @@ export class PetSignDesignComponent implements AfterViewInit, OnDestroy, OnInit,
         }
       }, 20);
     }
-
-    // Has to be separate from the previous interval since it has to be done loading
-    const loadHeight = setInterval(() => {
-      if (this.inputWrapper !== undefined && this.inputWrapper.nativeElement !== undefined) {
-        this.changeVerticalPlacement();
-        clearInterval(loadHeight);
-      }
-    }, 200);
   }
 
   ngAfterViewInit(): void {
@@ -141,7 +133,7 @@ export class PetSignDesignComponent implements AfterViewInit, OnDestroy, OnInit,
     const isDeprecatedSrc =
       !this.design.designSrc.includes('frame0-no-design.svg') &&
       this.design.designSrc.includes('assets/quotable/frame-design/frame');
-    // for depricated src links
+    // for deprecated src links
     if (isDeprecatedSrc) {
       return this.design.designSrc.replace('assets/quotable/frame-design/', 'assets/quotable/frame-design/quotable/');
     } else {
@@ -187,25 +179,6 @@ export class PetSignDesignComponent implements AfterViewInit, OnDestroy, OnInit,
     return Math.round(number * scale * 10) / 10;
   }
 
-  @HostListener('document:keydown.enter')
-  @HostListener('document:keydown.backspace')
-  reCenterText(): void {
-    // To recenter text after a new row has been created or deleted.
-    // Adding or removing a row doesnt trigger ngChange
-    setTimeout(() => {
-      this.changeVerticalPlacement();
-    }, 50);
-  }
-
-  changeVerticalPlacement() {
-    const textHeight = this.inputWrapper.nativeElement.offsetHeight;
-    const canvasHeight = this.designWrapper.nativeElement.offsetHeight;
-    const x = canvasHeight - textHeight;
-    const placement = this.design.verticalPlacement * (x / 100);
-
-    this.inputWrapper.nativeElement.style.top = 0 + 'px';
-  }
-
   /**
    * Adjust height of the input element to match its contents and amount fo rows. Based on the scroll height.
    */
@@ -234,10 +207,10 @@ export class PetSignDesignComponent implements AfterViewInit, OnDestroy, OnInit,
   }
 
   resizeText() {
-    // If undefined (depricated) set to true and resize input
+    // If undefined (deprecated) set to true and resize input
     if (this.design.showText === undefined) {
       this.design.showText = true;
-      // If show text is false, dont resize
+      // If show text is false, don't resize
     } else if (!this.design.showText) {
       return;
     }
@@ -248,11 +221,11 @@ export class PetSignDesignComponent implements AfterViewInit, OnDestroy, OnInit,
   }
 
   resizeTitle() {
-    // If undefined (depricated) set to false and dont resize input
+    // If undefined (deprecated) set to false and don't resize input
     if (this.design.showTitle === undefined) {
       this.design.showTitle = false;
       return;
-      // If show title is false, dont resize
+      // If show title is false, don't resize
     } else if (!this.design.showTitle) {
       return;
     }
@@ -265,9 +238,5 @@ export class PetSignDesignComponent implements AfterViewInit, OnDestroy, OnInit,
   triggerResize() {
     this.resizeText();
     this.resizeTitle();
-    // re adjust hight
-    setTimeout(() => {
-      this.changeVerticalPlacement();
-    }, 50);
   }
 }
